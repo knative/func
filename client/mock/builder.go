7f03a35e3b9401053968560af80a631f6680372a
Luke K <lkingland2038@gmail.com>
2020-04-09 16:30:43 +0000

    mock builder

---
+++ client/mock/builder.go
@@ -0,0 +1,17 @@
+package mock
+
+type Builder struct {
+	BuildInvoked bool
+	BuildFn      func(name, path string) (image string, err error)
+}
+
+func NewBuilder() *Builder {
+	return &Builder{
+		BuildFn: func(string, string) (string, error) { return "", nil },
+	}
+}
+
+func (i *Builder) Build(name, path string) (image string, err error) {
+	i.BuildInvoked = true
+	return i.BuildFn(name, path)
+}
