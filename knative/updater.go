8f65c8148d1fbe23ef43e79a16ecfbd1697bb266
Matej Vasek <mvasek@redhat.com>
2020-06-30 17:08:44 +0200

    fix: creating new revision of ksvc

--- knative/updater.go
+++ knative/updater.go
@@ -1,13 +1,18 @@
 package knative
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
+	"math/rand"
 	"sort"
+	"strings"
 	"time"
 
-	apiCoreV1 "k8s.io/api/core/v1"
+	. "k8s.io/api/core/v1"
 	apiMachineryV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
+	servingv1 "knative.dev/serving/pkg/apis/serving/v1"
 	servingV1client "knative.dev/serving/pkg/client/clientset/versioned/typed/serving/v1"
 
 	"github.com/boson-project/faas/k8s"
@@ -17,6 +22,7 @@ type Updater struct {
 	Verbose   bool
 	namespace string
 	client    *servingV1client.ServingV1Client
+
 }
 
 func NewUpdater(namespace string) (updater *Updater, err error) {
@@ -29,9 +35,6 @@ func NewUpdater(namespace string) (updater *Updater, err error) {
 		return
 	}
 	updater.client, err = servingV1client.NewForConfig(config)
-	if err != nil {
-		return
-	}
 	return
 }
 
@@ -53,26 +56,13 @@ func (updater *Updater) Update(name, image string) error {
 	}
 
 	container := &service.Spec.Template.Spec.Containers[0]
+	updateBuiltTimeStampEnvVar(container)
 
-	builtEnvVarName := "BUILT"
-	var builtEnvVar *apiCoreV1.EnvVar = nil
-	envs := container.Env
-	for i, envVar := range envs {
-		if envVar.Name == builtEnvVarName {
-			builtEnvVar = &envs[i]
-			break
-		}
+	service.Spec.Template.Name, err = generateRevisionName("{{.Service}}-{{.Random 5}}-{{.Generation}}", service)
+	if err != nil {
+		return fmt.Errorf("updater failed to generate revision name: %v", err)
 	}
-	if builtEnvVar == nil {
-		envs = append(envs, apiCoreV1.EnvVar{Name: builtEnvVarName})
-		builtEnvVar = &envs[len(envs)-1]
-	}
-	builtEnvVar.Value = time.Now().Format("20060102T150405")
 
-	sort.SliceStable(envs, func(i, j int) bool {
-		return envs[i].Name <= envs[j].Name
-	})
-	container.Env = envs
 
 	_, err = client.Services(namespace).Update(service)
 	if err != nil {
@@ -80,4 +70,77 @@ func (updater *Updater) Update(name, image string) error {
 	}
 
 	return nil
+}
+
+func updateBuiltTimeStampEnvVar(container *Container) {
+	builtEnvVarName := "BUILT"
+	envs := container.Env
+
+	builtEnvVar := findEnvVar(builtEnvVarName, envs)
+	if builtEnvVar == nil {
+		envs = append(envs, EnvVar{Name: builtEnvVarName, })
+		builtEnvVar = &envs[len(envs)-1]
+	}
+
+	builtEnvVar.Value = time.Now().Format("20060102T150405")
+
+	sort.SliceStable(envs, func(i, j int) bool {
+		return envs[i].Name <= envs[j].Name
+	})
+	container.Env = envs
+}
+
+func findEnvVar(name string, envs []EnvVar) *EnvVar {
+	var result *EnvVar = nil
+	for i, envVar := range envs {
+		if envVar.Name == name {
+			result = &envs[i]
+			break
+		}
+	}
+	return result
+}
+
+var charChoices = []string{
+	"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x",
+	"y", "z",
+}
+
+type revisionTemplContext struct {
+	Service    string
+	Generation int64
+}
+
+func (c *revisionTemplContext) Random(l int) string {
+	chars := make([]string, 0, l)
+	for i := 0; i < l; i++ {
+		chars = append(chars, charChoices[rand.Int()%len(charChoices)])
+	}
+	return strings.Join(chars, "")
+}
+
+func generateRevisionName(nameTempl string, service *servingv1.Service) (string, error) {
+	templ, err := template.New("revisionName").Parse(nameTempl)
+	if err != nil {
+		return "", err
+	}
+	context := &revisionTemplContext{
+		Service:    service.Name,
+		Generation: service.Generation + 1,
+	}
+	buf := new(bytes.Buffer)
+	err = templ.Execute(buf, context)
+	if err != nil {
+		return "", err
+	}
+	res := buf.String()
+	// Empty is ok.
+	if res == "" {
+		return res, nil
+	}
+	prefix := service.Name + "-"
+	if !strings.HasPrefix(res, prefix) {
+		res = prefix + res
+	}
+	return res, nil
 }
