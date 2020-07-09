5d6e2d3c8c018066734a34d6de9175b9e11326c1
Lance Ball <lball@redhat.com>
2020-07-09 15:05:26 -0400

    init: add Node.js HTTP template
    Note that this template currently points to boson-project/faas-js-runtime
    for it's framework dependency as it depends on currently unreleased features.
    Once 0.3.0 of that project is released, this template should be updated.
    This dependency is only needed for local testing and development.

---
+++ templates/node/http/local.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const runtime = require('@redhat/faas-js-runtime');
+const func = require('./index.js');
+const ON_DEATH = require('death')({ uncaughtException: true });
+
+runtime(func, server => {
+  ON_DEATH(_ => {
+    server.close();
+  });
+
+  console.log(`
+The server has started.
+
+You can use curl to GET or POST an event to the endpoint:
+
+curl -X GET http://localhost:8080?name=tiger
+
+curl -X POST -d '{"name": "tiger"}' http://localhost:8080 -H'Content-type: application/json'
+`);
+});
