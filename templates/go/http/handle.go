daf388dc7207c667dba6567b61531a28c8cd3dc1
Matej Vasek <mvasek@redhat.com>
2020-09-09 14:26:27 +0200

    fix: signature of http go function in template

--- templates/go/http/handle.go
+++ templates/go/http/handle.go
@@ -2,12 +2,20 @@ package function
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 // Handle an HTTP Request.
-func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) error {
+func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
+
 	res.Header().Add("Content-Type", "text/plain")
-	res.Write([]byte("OK\n"))
-	return nil
+	res.Header().Add("Content-Length", "3")
+	res.WriteHeader(200)
+
+	_, err := fmt.Fprintf(res, "OK\n")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error or response write: %v", err)
+	}
 }
