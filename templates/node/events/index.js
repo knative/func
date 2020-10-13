f26c579ccf95e713936fa8f247f4bb69be016214
Lance Ball <lball@redhat.com>
2020-10-13 14:57:39 -0400

    feat: introduce CloudEvent data as first parameter for event functions
    The 0.4.0 version of faas-js-runtime extracts the CloudEvent data from
    an incoming event and provides that as the first parameter when invoking
    a function which receives a CloudEvent. This commit bumps to that version
    as well as improves the overall readability and code documentation for the
    Node.js CloudEvent function.
    
    Signed-off-by: Lance Ball <lball@redhat.com>

--- templates/node/events/index.js
+++ templates/node/events/index.js
@@ -1,11 +1,34 @@
 'use strict';
 
-module.exports = async function (context) {
+/**
+ * An example function that responds to incoming CloudEvents over HTTP. For example,
+ * from the Knative event Broker. Try invoking with a request such as this.
+ *
+ * curl -X POST -d '{"name": "Tiger", "customerId": "0123456789"}' \
+ *  -H'Content-type: application/json' \
+ *  -H'Ce-id: 1' \
+ *  -H'Ce-source: cloud-event-example' \
+ *  -H'Ce-type: dev.knative.example' \
+ *  -H'Ce-specversion: 1.0' \
+ *  http://localhost:8080
+ *
+ * The event data is extracted from the incoming event and provided as the first
+ * parameter to the function. The CloudEvent object itself may be accessed via the
+ * context parameter, For example:
+ *
+ * const incomingEvent = context.cloudevent;
+ *
+ * @param {Object} customer the CloudEvent data. If the data content type is application/json
+ * this will be converted to an Object via JSON.parse()
+ * @param {Context} context the invocation context
+ */
+function processCustomer(customer, context) {
+  console.log(customer, context)
   if (!context.cloudevent) {
-    return Promise.reject(new Error('No cloud event received'));
+    return 'No cloud event received';
   }
-  context.log.info(`Cloud event received: ${JSON.stringify(context.cloudevent)}`);
-  return new Promise((resolve, reject) => {
-    setTimeout(_ => resolve({ data: context.cloudevent.data }), 500);
-  });
+  context.log.info('Processing customer', customer);
+  context.log.info(`CloudEvent received: ${context.cloudevent.toString()}`);
+  return { customer };
 };
+module.exports = processCustomer;
--- templates/node/events/test/unit.js
+++ templates/node/events/test/unit.js
@@ -7,17 +7,19 @@ const { CloudEvent } = require('cloudevents');
 // Ensure that the function completes cleanly when passed a valid event.
 test('Unit: handles a valid event', t => {
   t.plan(1);
+  const data = {
+    name: 'tiger',
+    customerId: '01234'
+  }
   // A valid event includes id, type and source at a minimum.
   const cloudevent = new CloudEvent({
-    id: 'TEST-EVENT-01',
+    id: '01234',
     type: 'com.example.cloudevents.test',
     source: '/test',
-    data: {
-      message: 'hello'
-    }
+    data
   });
 
   // Invoke the function with the valid event, which should compelte without error.
-  t.ok(func({ cloudevent, log: { info: console.log } }));
+  t.ok(func(data, { cloudevent, log: { info: console.log } }));
   t.end();
 });
