00e5d187c292e7dd684c966b063c5b24e48ecdc2
Zbynek Roubalik <726523+zroubalik@users.noreply.github.com>
2020-10-19 11:43:50 +0200

    src: use image digest (#175)
    Signed-off-by: Zbynek Roubalik <zroubali@redhat.com>

--- docker/pusher.go
+++ docker/pusher.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/boson-project/faas"
 )
@@ -22,7 +24,7 @@ func NewPusher() *Pusher {
 }
 
 // Push the image of the Function.
-func (n *Pusher) Push(f faas.Function) (err error) {
+func (n *Pusher) Push(f faas.Function) (digest string, err error) {
 	// Check for the docker binary explicitly so that we can return
 	// an extra-friendly error message.
 	_, err = exec.LookPath("docker")
@@ -32,17 +34,22 @@ func (n *Pusher) Push(f faas.Function) (err error) {
 	}
 
 	if f.Image == "" {
-		return errors.New("Function has no associated image.  Has it been built?")
+		return "", errors.New("Function has no associated image.  Has it been built?")
 	}
 
 	// set up the command, specifying a sanitized project name and connecting
 	// standard output and error.
 	cmd := exec.Command("docker", "push", f.Image)
 
+	// Capture the command output in the buffer
+	var output bytes.Buffer
+
 	// If verbose logging is enabled, echo chatty stdout.
 	if n.Verbose {
 		fmt.Println(cmd)
-		cmd.Stdout = os.Stdout
+		cmd.Stdout = io.MultiWriter(&output, os.Stdout)
+	} else {
+		cmd.Stdout = &output
 	}
 
 	// Capture stderr for echoing on failure.
@@ -55,5 +62,32 @@ func (n *Pusher) Push(f faas.Function) (err error) {
 		// TODO: sanitize stderr from docker?
 		err = fmt.Errorf("%v. %v", stderr.String(), err.Error())
 	}
+
+	digest = parseDigest(output.String())
+
 	return
+}
+
+// parseDigest tries to parse the last line from the output, which holds the pushed image digest
+// The last line should look like this:
+// latest: digest: sha256:a278a91112d17f8bde6b5f802a3317c7c752cf88078dae6f4b5a0784deb81782 size: 2613
+func parseDigest(output string) string {
+
+	// get last line from the output
+	lines := strings.Split(output, "\n")
+	lastline := lines[len(lines)-2]
+
+	// find the start index of the "digest" section
+	shaIndex := strings.Index(lastline, "sha256")
+	if shaIndex == -1 {
+		return ""
+	}
+	subStr := lastline[shaIndex:]
+
+	// find the end index of the "digest" section
+	endIndex := strings.Index(subStr, " ")
+	if endIndex == -1 {
+		return ""
+	}
+	return subStr[:endIndex]
 }
