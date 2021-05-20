fad522d4a396feb53c4272084dfce58cbe191f3a
Jim Crossley <jim@crossleys.org>
2021-05-20 18:16:14 -0400

    Expect the event parameter to be the entire CloudEvent
    This will require a change to the faas-js-runtime, but at least the
    template won't need to change when that's released

--- templates/node/events/index.js
+++ templates/node/events/index.js
@@ -15,21 +15,16 @@ const { CloudEvent, HTTP } = require('cloudevents');
  *  -H'Ce-specversion: 1.0' \
  *  http://localhost:8080
  *
- * For convenience, the runtime will extract the event data from the
- * CloudEvent envelope and pass it in the second parameter to the
- * function. The entire envelope, along with the HTTP request
- * metadata, is available from the first parameter, 'context'.
- *
  * @param {Context} context the invocation context
- * @param {Object} event the CloudEvent data
+ * @param {Object} event the CloudEvent 
  */
 function handle(context, event) {
 
+  context.log.info("context");
+  console.log(JSON.stringify(context, null, 2));
+
   context.log.info("event");
   console.log(JSON.stringify(event, null, 2));
-
-  context.log.info("cloudevent envelope");
-  console.log(JSON.stringify(context.cloudevent, null, 2));
 
   return HTTP.binary(new CloudEvent({
     source: 'event.handler',
