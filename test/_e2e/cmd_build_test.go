0b74befda19031839f2d9fc6035f5d09514fddd0
Jefferson Ramos <jrangelramos@gmail.com>
2021-08-23 11:32:55 -0300

    test: adding e2e test for remote repository

---
+++ test/_e2e/cmd_build_test.go
@@ -0,0 +1,26 @@
+package e2e
+
+import (
+	"regexp"
+	"testing"
+)
+
+// Build runs `func build' command for a given test project.
+func Build(t *testing.T, knFunc *TestShellCmdRunner, project *FunctionTestProject)  {
+
+	result := knFunc.Exec("build", "--path", project.ProjectPath, "--registry", GetRegistry())
+	if result.Error != nil {
+		t.Fail()
+	}
+
+	// Remove some unwanted control chars to make easy to parse the result
+	cleanStdout := CleanOutput(result.Stdout)
+
+	// Build command displays a succeed message followed by the image name derived
+	wasBuilt := regexp.MustCompile("Function image built:").MatchString(cleanStdout)
+	if !wasBuilt {
+		t.Fatal("Function was not built")
+	}
+	project.IsBuilt = true
+
+}
