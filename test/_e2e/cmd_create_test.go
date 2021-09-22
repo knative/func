6f39cbd4e10052d0ee6da983770e0bda3943fcb3
Luke Kingland <lkingland2038@gmail.com>
2021-09-22 21:45:32 +0900

    e2e: update tests for language flag

--- test/_e2e/cmd_create_test.go
+++ test/_e2e/cmd_create_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // Create runs `func create' command for a given test project with basic validation
 func Create(t *testing.T, knFunc *TestShellCmdRunner, project FunctionTestProject) {
-	result := knFunc.Exec("create", project.ProjectPath, "--runtime", project.Runtime, "--template", project.Template)
+	result := knFunc.Exec("create", project.ProjectPath, "--language", project.Runtime, "--template", project.Template)
 	if result.Error != nil {
 		t.Fatal()
 	}
