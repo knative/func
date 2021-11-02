d7dbebb7475c58f76694a5fcf9e6bd0beea6f28a
David Simansky <dsimansk@redhat.com>
2021-11-02 17:38:41 +0100

    Update Knative to 0.25 + cherry-pick E2E refactor (#617)
    * ci: split pull_requests workflow
    
    * ci: make binary for e2e tests
    
    * Update Knative to 0.25 release
    
    * Update pkged file
    
    * Update pkged file
    
    * ci: Install Bash 4 to MacOS unit tests
    
    * ci: Fixup java version
    
    Co-authored-by: Luke Kingland <lkingland2038@gmail.com>

--- vendor/knative.dev/client/pkg/kn/plugin/manager.go
+++ vendor/knative.dev/client/pkg/kn/plugin/manager.go
@@ -52,6 +52,26 @@ type Plugin interface {
 	Path() string
 }
 
+type ContextData map[string]string
+
+type PluginManifest struct {
+	// ProducesContextDataKeys is a list of keys for the ContextData that
+	// a plugin can produce. Nil or an empty list declares that this
+	// plugin is not ContextDataProducer
+	ProducesContextDataKeys []string
+
+	// ConsumesContextDataKeys is a list of keys from a ContextData that a
+	// plugin is interested in to consume. Nil or an empty list declares
+	// that this plugin is not a ContextDataConsumer
+	ConsumesContextDataKeys []string
+}
+
+type ContextDataConsumer interface {
+	// ExecuteWithContextData executes the plugin with the given args much like
+	// Execute() but with an additional argument that holds the ContextData
+	ExecuteWithContextData(args []string, data ContextData) error
+}
+
 type Manager struct {
 	// Dedicated plugin directory as configured
 	pluginsDir string
@@ -381,6 +401,11 @@ func findMostSpecificPluginInPath(dir string, parts []string, lookupInPath bool)
 		var nameParts []string
 		var commandParts []string
 		for _, p := range parts[0:i] {
+			// for arguments that contain the path separator,
+			// stop the loop once the separator appears
+			if strings.Contains(p, string(os.PathSeparator)) {
+				break
+			}
 			// Subcommands with "-" are translated to "_"
 			// (e.g. a command  "kn log-all" is translated to a plugin "kn-log_all")
 			nameParts = append(nameParts, convertDashToUnderscore(p))
