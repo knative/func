fba4685e6c3876dd0ba83f404d6e586983087670
Fabian Lopez <lfabian@vmware.com>
2021-11-29 21:29:27 -0500

    add unit test to build command

--- cmd/build_test.go
+++ cmd/build_test.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/ory/viper"
 	fn "knative.dev/kn-plugin-func"
 	"knative.dev/kn-plugin-func/mock"
 )
@@ -47,5 +50,70 @@ created: 2021-01-01T00:00:00+00:00
 	err := cmd.Execute()
 	if err == nil {
 		t.Fatal("Expected error")
+	}
+}
+
+func Test_runBuild(t *testing.T) {
+	tests := []struct {
+		name         string
+		pushFlag     bool
+		fileContents string
+		shouldBuild  bool
+		shouldPush   bool
+		wantErr      bool
+	}{
+		{
+			name:     "asdf",
+			pushFlag: true,
+			fileContents: `name: test-func
+runtime: go`,
+			shouldBuild: true,
+			shouldPush:  true,
+		},
+	}
+	for _, tt := range tests {
+		mockPusher := mock.NewPusher()
+		mockBuilder := mock.NewBuilder()
+		cmd := NewBuildCmd(func(bc buildConfig) (*fn.Client, error) {
+			return fn.New(
+				fn.WithBuilder(mockBuilder),
+				fn.WithPusher(mockPusher),
+			), nil
+		})
+
+		tempDir, err := os.MkdirTemp("", "func-tests")
+		if err != nil {
+			t.Fatalf("temp dir couldn't be created %v", err)
+		}
+		t.Log("tempDir created:", tempDir)
+		t.Cleanup(func() {
+			os.RemoveAll(tempDir)
+		})
+
+		fullPath := tempDir + "/func.yaml"
+		tempFile, err := os.Create(fullPath)
+		if err != nil {
+			t.Fatalf("temp file couldn't be created %v", err)
+		}
+		_, err = tempFile.WriteString(tt.fileContents)
+		if err != nil {
+			t.Fatalf("file content was not written %v", err)
+		}
+
+		cmd.SetArgs([]string{"--path=" + tempDir})
+		viper.SetDefault("push", tt.pushFlag)
+
+		t.Run(tt.name, func(t *testing.T) {
+			err := cmd.Execute()
+			fmt.Printf("ERROR: %+v\n", err)
+
+			if mockBuilder.BuildInvoked != tt.shouldBuild {
+				t.Errorf("Build execution expected: %v but was actually %v", tt.shouldBuild, mockBuilder.BuildInvoked)
+			}
+
+			if mockPusher.PushInvoked != tt.shouldPush {
+				t.Errorf("Push execution expected: %v but was actually %v", tt.shouldPush, mockPusher.PushInvoked)
+			}
+		})
 	}
 }
