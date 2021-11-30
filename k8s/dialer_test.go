4adccc60589db259f5b2c650a86c704ab5e8d2bc
Matej Vasek <mvasek@redhat.com>
2021-11-30 20:49:49 +0100

    fixup: decrease test iterations
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- k8s/dialer_test.go
+++ k8s/dialer_test.go
@@ -132,7 +132,7 @@ func TestDialInClusterService(t *testing.T) {
 	}
 
 	var wg sync.WaitGroup
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
