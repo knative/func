52649534eef827cd68e9d03c62afb79a814b0c46
Lance Ball <lball@redhat.com>
2021-12-20 17:04:53 -0500

    chore: add knative infra release scripts
    Signed-off-by: Lance Ball <lball@redhat.com>

---
+++ hack/release.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Documentation about this script and how to use it can be found
+# at https://github.com/knative/hack
+ORG_NAME=knative-sandbox
+VALIDATION_TESTS="make test"
+
+source $(dirname $0)/../vendor/knative.dev/hack/release.sh
+
+function build_release() {
+  echo "🚧 🐧 Building cross platform binaries: Linux 🐧 (amd64), MacOS 🍏, and Windows 🎠"
+  make cross-platform
+
+  ARTIFACTS_TO_PUBLISH="func_darwin_amd64 func_linux_amd64 func_windows_amd64.exe"
+  sha256sum ${ARTIFACTS_TO_PUBLISH} > checksums.txt
+  ARTIFACTS_TO_PUBLISH="${ARTIFACTS_TO_PUBLISH} checksums.txt"
+  echo "🧮     Checksum:"
+  cat checksums.txt
+}
+
+main $@
