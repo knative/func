b397993ee50178d830323a8a02bb9cc7304ea5c4
David Simansky <dsimansk@redhat.com>
2022-03-30 15:09:33 +0200

    Fix docker push test

--- docker/pusher_test.go
+++ docker/pusher_test.go
@@ -178,6 +178,10 @@ func TestNonDaemonPush(t *testing.T) {
 		return f, nil
 	}
 
+	dockerClient.imageInspect = func(ctx context.Context, s string) (types.ImageInspect, []byte, error) {
+		return types.ImageInspect{ID: "sha256:ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"}, []byte{}, nil
+	}
+
 	dockerClientFactory := func() (docker.PusherDockerClient, error) {
 		return dockerClient, nil
 	}
@@ -282,6 +286,7 @@ type mockPusherDockerClient struct {
 	negotiateAPIVersion func(ctx context.Context)
 	imagePush           func(ctx context.Context, ref string, options types.ImagePushOptions) (io.ReadCloser, error)
 	imageSave           func(ctx context.Context, strings []string) (io.ReadCloser, error)
+	imageInspect        func(ctx context.Context, s string) (types.ImageInspect, []byte, error)
 	close               func() error
 }
 
@@ -299,6 +304,11 @@ func (m *mockPusherDockerClient) ImageLoad(ctx context.Context, reader io.Reader
 
 func (m *mockPusherDockerClient) ImageTag(ctx context.Context, s string, s2 string) error {
 	panic("implement me")
+}
+
+func (m *mockPusherDockerClient) ImageInspectWithRaw(ctx context.Context, s string) (types.ImageInspect, []byte, error) {
+	return m.imageInspect(ctx, s)
+	//return types.ImageInspect{ID: "1", Config: &container.Config{Image: "1"}}, []byte{}, nil
 }
 
 func (m *mockPusherDockerClient) ImagePush(ctx context.Context, ref string, options types.ImagePushOptions) (io.ReadCloser, error) {
