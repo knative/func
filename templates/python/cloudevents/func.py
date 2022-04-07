677314e6f79ba1633f2ee20ea8d88ab5e2d8abb3
Lance Ball <lball@redhat.com>
2022-04-07 13:52:32 -0400

    fixup: fix whitespace
    Signed-off-by: Lance Ball <lball@redhat.com>

--- templates/python/cloudevents/func.py
+++ templates/python/cloudevents/func.py
@@ -8,7 +8,7 @@ def main(context: Context):
     The context parameter contains the Flask request object and any
     CloudEvent received with the request.
     """
-    
+
     # Add your business logic here
 
     # The return value here will be applied as the data attribute
