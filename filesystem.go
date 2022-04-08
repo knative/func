03794ba9b8931fb7e21efe24736e85b8ad7ba08d
Matej Vasek <mvasek@redhat.com>
2022-04-08 14:10:57 +0200

    Refactor: use fs.WalkDir to copy from FS
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- filesystem.go
+++ filesystem.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"sort"
 	"strings"
 
 	billy "github.com/go-git/go-billy/v5"
@@ -148,67 +147,50 @@ func (o osFilesystem) Stat(name string) (fs.FileInfo, error) {
 // copy
 
 func copy(src, dest string, accessor Filesystem) (err error) {
-	node, err := accessor.Stat(src)
-	if err != nil {
-		return
-	}
-	if node.IsDir() {
-		return copyNode(src, dest, accessor)
-	} else {
-		return copyLeaf(src, dest, accessor)
-	}
-}
 
-func copyNode(src, dest string, accessor Filesystem) (err error) {
-	// Ideally we should use the file mode of the src node
-	// but it seems the git module is reporting directories
-	// as 0644 instead of 0755. For now, just do it this way.
-	// See https://github.com/go-git/go-git/issues/364
-	// Upon resolution, return accessor.Stat(src).Mode()
-	err = os.MkdirAll(dest, 0755)
-	if err != nil {
-		return
-	}
-
-	children, err := readDir(src, accessor)
-	if err != nil {
-		return
-	}
-	for _, child := range children {
-		if err = copy(path.Join(src, child.Name()), filepath.Join(dest, child.Name()), accessor); err != nil {
-			return
+	return fs.WalkDir(accessor, src, func(path string, de fs.DirEntry, err error) error {
+		if err != nil {
+			return err
 		}
-	}
-	return
-}
 
-func readDir(src string, accessor Filesystem) ([]fs.DirEntry, error) {
-	list, err := accessor.ReadDir(src)
-	if err != nil {
-		return nil, err
-	}
-	sort.Slice(list, func(i, j int) bool { return list[i].Name() < list[j].Name() })
-	return list, nil
-}
+		if path == src {
+			return nil
+		}
 
-func copyLeaf(src, dest string, accessor Filesystem) (err error) {
-	srcFile, err := accessor.Open(src)
-	if err != nil {
-		return
-	}
-	defer srcFile.Close()
+		p, err := filepath.Rel(filepath.FromSlash(src), filepath.FromSlash(path))
+		if err != nil {
+			return err
+		}
 
-	srcFileInfo, err := accessor.Stat(src)
-	if err != nil {
-		return
-	}
+		dest := filepath.Join(dest, p)
+		if de.IsDir() {
+			// Ideally we should use the file mode of the src node
+			// but it seems the git module is reporting directories
+			// as 0644 instead of 0755. For now, just do it this way.
+			// See https://github.com/go-git/go-git/issues/364
+			// Upon resolution, return accessor.Stat(src).Mode()
+			return os.MkdirAll(dest, 0755)
+		} else {
+			fi, err := de.Info()
+			if err != nil {
+				return err
+			}
 
-	destFile, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE|os.O_TRUNC, srcFileInfo.Mode())
-	if err != nil {
-		return
-	}
-	defer destFile.Close()
+			destFile, err := os.OpenFile(dest, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fi.Mode())
+			if err != nil {
+				return err
+			}
+			defer destFile.Close()
 
-	_, err = io.Copy(destFile, srcFile)
-	return
+			srcFile, err := accessor.Open(path)
+			if err != nil {
+				return err
+			}
+			defer srcFile.Close()
+
+			_, err = io.Copy(destFile, srcFile)
+			return err
+		}
+	})
+
 }
