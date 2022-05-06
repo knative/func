cc3988b5a6a9f5a1ee0979114d4c420093849895
Luke Kingland <lkingland2038@gmail.com>
2022-05-06 16:03:48 +0900

    feat: improved invoke verbosity

--- templates/go/cloudevents/handle_test.go
+++ templates/go/cloudevents/handle_test.go
@@ -2,43 +2,31 @@ package function
 
 import (
 	"context"
-	"encoding/json"
 	"testing"
-
-	cloudevents "github.com/cloudevents/sdk-go/v2"
 
 	"github.com/cloudevents/sdk-go/v2/event"
 )
 
 // TestHandle ensures that Handle accepts a valid CloudEvent without error.
 func TestHandle(t *testing.T) {
-	// A minimal, but valid, event.
-	event := event.New()
-	event.SetID("TEST-EVENT-01")
-	event.SetType("MyEvent")
-	event.SetSource("http://localhost:8080/")
-	event.SetSubject("Echo")
-	input := Echo{Message: "hello"}
-	event.SetData(cloudevents.ApplicationJSON, input)
-	// Invoke the defined handler.
-	ce, err := Handle(context.Background(), event)
+	// Assemble
+	e := event.New()
+	e.SetID("id")
+	e.SetType("type")
+	e.SetSource("source")
+	e.SetData("text/plain", "data")
+
+	// Act
+	echo, err := Handle(context.Background(), e)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if ce == nil {
-		t.Errorf("The output CloudEvent cannot be nil")
+	// Assert
+	if echo == nil {
+		t.Errorf("received nil event") // fail on nil
 	}
-	if ce.Type() != "Echo" {
-		t.Errorf("Wrong CloudEvent Type received: %v , expected Echo", ce.Type())
+	if string(echo.Data()) != "data" {
+		t.Errorf("the received event expected data to be 'data', got '%s'", echo.Data())
 	}
-	var output Echo
-	err = json.Unmarshal(ce.Data(), &output)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if expected := "echo " + input.Message; output.Message != expected {
-		t.Errorf("The expected output should be: %v, and it was: %v", expected, output.Message)
-	}
-
 }
--- templates/go/http/handle.go
+++ templates/go/http/handle.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // Handle an HTTP Request.
@@ -14,7 +15,27 @@ func Handle(ctx context.Context, res http.ResponseWriter, req *http.Request) {
 	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
 	 */
 
-	// Example implementation:
-	fmt.Println("OK")       // Print "OK" to standard output (local logs)
-	fmt.Fprintln(res, "OK") // Send "OK" back to the client
+	fmt.Println("Received request")
+	fmt.Println(prettyPrint(req))      // echo to local output
+	fmt.Fprintf(res, prettyPrint(req)) // echo to caller
+}
+
+func prettyPrint(req *http.Request) string {
+	b := &strings.Builder{}
+	fmt.Fprintf(b, "%v %v %v %v\n", req.Method, req.URL, req.Proto, req.Host)
+	for k, vv := range req.Header {
+		for _, v := range vv {
+			fmt.Fprintf(b, "  %v: %v\n", k, v)
+		}
+	}
+
+	if req.Method == "POST" {
+		req.ParseForm()
+		fmt.Fprintln(b, "Body:")
+		for k, v := range req.Form {
+			fmt.Fprintf(b, "  %v: %v\n", k, v)
+		}
+	}
+
+	return b.String()
 }
