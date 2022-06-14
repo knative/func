44006c4811365246f2f774ab77388a98d235ef00
salaboy <Salaboy@gmail.com>
2022-06-14 17:04:49 +0100

    removing space

--- templates/springboot/http/src/test/java/functions/UpperCaseFunctionTest.java
+++ templates/springboot/http/src/test/java/functions/UpperCaseFunctionTest.java
@@ -10,7 +10,7 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;  
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(classes = CloudFunctionApplication.class,
   webEnvironment = WebEnvironment.RANDOM_PORT)
