ebe6ef7457860b56976f3f7e96d7893e31c27f8e
mcruzdev <matheuscruz.dev@gmail.com>
2022-06-17 10:39:27 -0300

    refactor: regenerate zz file

--- templates/springboot/http/src/main/java/functions/CloudFunctionApplication.java
+++ templates/springboot/http/src/main/java/functions/CloudFunctionApplication.java
@@ -3,13 +3,11 @@ package functions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 
 import java.util.function.Function;
 
 @SpringBootApplication
-@Configuration
 public class CloudFunctionApplication {
 
   public static void main(String[] args) {
