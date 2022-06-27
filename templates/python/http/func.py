062ffbdf8002936dc390afc6236fa76db5fe5c99
Daniel Bodky <dbodky@gmail.com>
2022-06-27 21:39:45 +0200

    Updated output to dict-only

--- templates/python/http/func.py
+++ templates/python/http/func.py
@@ -2,10 +2,43 @@ from parliament import Context
 from flask import Request
 
 
+# parse request body, json data or URL query parameters
+def payload_print(req: Request) -> str:
+    if req.method == "POST":
+        if req.is_json:
+            return str(req.json)
+        else:
+            # MultiDict needs some iteration
+            ret = "{"
+
+            for key in req.form.keys():
+                ret += "'" + key + "': '" + req.form[key] + "', "
+
+            return ret[:-2] + "}\n" if len(ret) > 2 else "{}"
+
+    elif req.method == "GET":
+        # MultiDict needs some iteration
+        ret = "{"
+
+        for key in req.args.keys():
+            ret += "'" + key + "': '" + req.args[key] + "', "
+
+        return ret[:-2] + "}\n" if len(ret) > 2 else "{}"
+
+
+# pretty print the request to stdout instantaneously
 def pretty_print(req: Request) -> str:
     ret = str(req.method) + ' ' + str(req.url) + ' ' + str(req.host) + '\n'
     for (header, values) in req.headers:
-        ret += str(header) + ": " + values + '\n'
+        ret += "  " + str(header) + ": " + values + '\n'
+
+    if req.method == "POST":
+        ret += "Request body:\n"
+        ret += "  " + payload_print(req) + '\n'
+
+    elif req.method == "GET":
+        ret += "URL Query String:\n"
+        ret += "  " + payload_print(req) + '\n'
 
     return ret
 
@@ -22,8 +55,8 @@ def main(context: Context):
 
     if 'request' in context.keys():
         ret = pretty_print(context.request)
-        print(ret)
-        return { "message": "hello" }, 200
+        print(ret, flush=True)
+        return payload_print(context.request), 200
     else:
-        print("Empty request")
-        return { "message": "Empty request" }, 200
+        print("Empty request", flush=True)
+        return "{}", 200
--- templates/python/http/test_func.py
+++ templates/python/http/test_func.py
@@ -6,7 +6,7 @@ class TestFunc(unittest.TestCase):
 
   def test_func_empty_request(self):
     resp, code = func.main({})
-    self.assertEqual(resp["message"], "Empty request")
+    self.assertEqual(resp, "{}")
     self.assertEqual(code, 200)
 
 if __name__ == "__main__":
