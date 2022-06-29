b85d0efa301b1aec59277e7c29bc9da91ee995ed
Lance Ball <lball@redhat.com>
2022-06-29 09:58:04 -0400

    fixup: tweak title formatting
    Signed-off-by: Lance Ball <lball@redhat.com>

--- hack/build-docs.sh
+++ hack/build-docs.sh
@@ -4,7 +4,9 @@ BIN=./func
 COMMANDS=("create" "build" "config" "create" "delete" "deploy" "info" "invoke" "list" "repository" "run")
 DOC=docs/reference/commands.txt
 
-echo "Command Reference" > ${DOC}
+echo "CLI Command Reference" > ${DOC}
+echo >> ${DOC}
+echo "------------------------------------------------------------------------------" >> ${DOC}
 echo >> ${DOC}
 
 for cmd in ${COMMANDS[@]}; do
