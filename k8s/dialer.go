3c9f910b54458c57d1e37015d76504f4fbda3a53
Matej Vasek <mvasek@redhat.com>
2022-08-31 03:17:26 +0200

    fixup: seccomp
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- k8s/dialer.go
+++ k8s/dialer.go
@@ -149,6 +149,7 @@ func (c *contextDialer) startDialerPod(ctx context.Context) (err error) {
 						AllowPrivilegeEscalation: new(bool),
 						RunAsNonRoot:             &runAsNonRoot,
 						Capabilities:             &coreV1.Capabilities{Drop: []coreV1.Capability{"ALL"}},
+						SeccompProfile:           &coreV1.SeccompProfile{Type: coreV1.SeccompProfileTypeRuntimeDefault},
 					},
 				},
 			},
