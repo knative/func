dec55af494fcef1c586e30ed022ffb6532af44a2
Matej Vasek <mvasek@redhat.com>
2022-09-08 17:07:58 +0200

    fix: set seccomp profile for dialer pod
    This is required by OpenShift 4.12.
    
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- k8s/dialer.go
+++ k8s/dialer.go
@@ -150,6 +150,7 @@ func (c *contextDialer) startDialerPod(ctx context.Context) (err error) {
 						AllowPrivilegeEscalation: new(bool),
 						RunAsNonRoot:             &runAsNonRoot,
 						Capabilities:             &coreV1.Capabilities{Drop: []coreV1.Capability{"ALL"}},
+						SeccompProfile:           &coreV1.SeccompProfile{Type: coreV1.SeccompProfileTypeRuntimeDefault},
 					},
 				},
 			},
