6f62273e2c6c8b137ab4032009eea8c9b8242565
Matej Vasek <mvasek@redhat.com>
2022-09-13 22:00:43 +0200

    fixup: improved tests
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- cmd/socat/main_test.go
+++ cmd/socat/main_test.go
@@ -13,24 +13,39 @@ import (
 
 func TestRootCmd(t *testing.T) {
 
-	/* Begin prepare TCP server and the file begin */
+	/* Begin prepare TCP server and the files */
 	addr := startTCPEcho(t)
 
-	const fileContent = "file-content"
+	const testData = "file-content\n"
 	tmpDir := t.TempDir()
-	testFile := filepath.Join(tmpDir, "a.txt")
-	err := os.WriteFile(testFile, []byte(fileContent), 0644)
+	inputFile := filepath.Join(tmpDir, "a.txt")
+	err := os.WriteFile(inputFile, []byte(testData), 0644)
 	if err != nil {
 		t.Fatal(err)
 	}
-	/* End prepare TCP server and the file begin */
+
+	outputFile := filepath.Join(tmpDir, "b.txt")
+	err = os.WriteFile(outputFile, []byte{}, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	/* End prepare TCP server and the files */
+
+	type matcher = func(string) bool
+	contains := func(pattern string) func(string) bool {
+		return func(s string) bool { return strings.Contains(s, pattern) }
+	}
+	equalsTo := func(pattern string) func(string) bool {
+		return func(s string) bool { return s == pattern }
+	}
 
 	type args struct {
-		args          []string
-		inputString   string
-		outMatcher    func(string) bool
-		errOutMatcher func(string) bool
-		wantErr       bool
+		args           []string
+		inputString    string
+		outMatcher     matcher
+		errOutMatcher  matcher
+		outFileMatcher matcher
+		wantErr        bool
 	}
 	tests := []struct {
 		name string
@@ -39,10 +54,17 @@ func TestRootCmd(t *testing.T) {
 		{
 			name: "stdio<->tcp",
 			args: args{
-				args:          []string{"-", "TCP:" + addr},
-				inputString:   "tcp-echo",
-				outMatcher:    func(s string) bool { return s == "tcp-echo" },
-				errOutMatcher: func(s string) bool { return true },
+				args:        []string{"-", "TCP:" + addr},
+				inputString: testData,
+				outMatcher:  equalsTo(testData),
+			},
+		},
+		{
+			name: "tcp<->stdio",
+			args: args{
+				args:        []string{"TCP:" + addr, "-"},
+				inputString: testData,
+				outMatcher:  equalsTo(testData),
 			},
 		},
 		{
@@ -50,18 +72,24 @@ func TestRootCmd(t *testing.T) {
 			args: args{
 				args:          []string{"-", "TCP:does.not.exist:10000"},
 				inputString:   "tcp-echo",
-				outMatcher:    func(s string) bool { return true },
-				errOutMatcher: func(s string) bool { return strings.Contains(s, "not resolve") },
+				errOutMatcher: contains("not resolve"),
 				wantErr:       true,
 			},
 		},
 		{
 			name: "file->stdio",
 			args: args{
-				args:          []string{"-u", "OPEN:" + testFile, "-"},
-				inputString:   "",
-				outMatcher:    func(s string) bool { return s == fileContent },
-				errOutMatcher: func(s string) bool { return true },
+				args:        []string{"-u", "OPEN:" + inputFile, "-"},
+				inputString: "",
+				outMatcher:  equalsTo(testData),
+			},
+		},
+		{
+			name: "stdio->file",
+			args: args{
+				args:           []string{"-u", "-", "OPEN:" + outputFile},
+				inputString:    testData,
+				outFileMatcher: equalsTo(testData),
 			},
 		},
 	}
@@ -69,10 +97,12 @@ func TestRootCmd(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			var out, errOut bytes.Buffer
 
+			stdout := &testWriter{Writer: &out}
+			stderr := &testWriter{Writer: &errOut}
 			cmd := NewRootCmd()
 			cmd.SetIn(io.NopCloser(strings.NewReader(tt.args.inputString)))
-			cmd.SetOut(noopWriterCloser{&out})
-			cmd.SetErr(noopWriterCloser{&errOut})
+			cmd.SetOut(stdout)
+			cmd.SetErr(stderr)
 			cmd.SetArgs(tt.args.args)
 
 			err = cmd.Execute()
@@ -84,22 +114,31 @@ func TestRootCmd(t *testing.T) {
 			if err == nil && tt.args.wantErr {
 				t.Error("expected error but got nil")
 			}
-
-			if !tt.args.outMatcher(out.String()) {
-				t.Error("bad output")
+			
+			if tt.args.outMatcher != nil && !tt.args.outMatcher(out.String()) {
+				t.Error("bad standard output")
 			}
-			if !tt.args.errOutMatcher(errOut.String()) {
-				t.Error("bat error output")
+			if tt.args.errOutMatcher != nil && !tt.args.errOutMatcher(errOut.String()) {
+				t.Error("bad standard error output")
+			}
+			if tt.args.outFileMatcher != nil {
+				bs, e := os.ReadFile(outputFile)
+				if e != nil {
+					t.Fatal(e)
+				}
+				if !tt.args.outFileMatcher(string(bs)) {
+					t.Error("bad content of the output file")
+				}
 			}
 		})
 	}
 }
 
-type noopWriterCloser struct {
+type testWriter struct {
 	io.Writer
 }
 
-func (n noopWriterCloser) Close() error {
+func (n *testWriter) Close() error {
 	return nil
 }
 
