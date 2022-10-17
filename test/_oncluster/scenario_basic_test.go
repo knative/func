46495ba05d8c027519ee387ecc27aba202f693d2
Matej Vasek <mvasek@redhat.com>
2022-10-17 10:45:13 +0200

    test: e2e test for direct upload on-cluster-build
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- test/_oncluster/scenario_basic_test.go
+++ test/_oncluster/scenario_basic_test.go
@@ -14,11 +14,57 @@ import (
 	e2e "knative.dev/func/test/_e2e"
 )
 
+// TestBasicUpload check if direct source upload works
+func TestBasicUpload(t *testing.T) {
+
+	var funcName = "test-func-basic-upload"
+	var funcPath = filepath.Join(t.TempDir(), funcName)
+
+	func() {
+		knFunc := common.NewKnFuncShellCli(t)
+		knFunc.Exec("create", "-l", "node", funcPath)
+
+		// Write an `index.js` that make node func to return 'first revision'
+		WriteNewSimpleIndexJS(t, funcPath, "first revision")
+
+		// Deploy it
+		knFunc.Exec("deploy",
+			"-p", funcPath,
+			"-r", e2e.GetRegistry(),
+			"--remote",
+		)
+		defer knFunc.Exec("delete", "-p", funcPath)
+
+		// Assert "first revision" is returned
+		result := knFunc.Exec("invoke", "-p", funcPath)
+		assert.Assert(t, strings.Contains(result.Stdout, "first revision"), "Func body does not contain 'first revision'")
+
+		previousServiceRevision := e2e.GetCurrentServiceRevision(t, funcName)
+
+		// Update index.js to force node func to return 'new revision'
+		WriteNewSimpleIndexJS(t, funcPath, "new revision")
+
+		// Re-Deploy Func
+		knFunc.Exec("deploy",
+			"-r", e2e.GetRegistry(),
+			"-p", funcPath,
+			"--remote")
+		e2e.NewRevisionCheck(t, previousServiceRevision, funcName) // Wait New Service Revision
+
+		// -- Assertions --
+		result = knFunc.Exec("invoke", "-p", funcPath)
+		assert.Assert(t, strings.Contains(result.Stdout, "new revision"), "Func body does not contain 'new revision'")
+		AssertThatTektonPipelineRunSucceed(t, funcName)
+	}()
+
+	AssertThatTektonPipelineResourcesNotExists(t, funcName)
+}
+
 // TestDefault covers basic test scenario that ensure on cluster build from a "default branch" and
 // code changes (new commits) will be properly built and deployed on new revision
-func TestBasicDefault(t *testing.T) {
+func TestBasicGit(t *testing.T) {
 
-	var funcName = "test-func-basic"
+	var funcName = "test-func-basic-git"
 	var funcPath = filepath.Join(t.TempDir(), funcName)
 
 	func() {
