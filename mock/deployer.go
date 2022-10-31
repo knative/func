2fa4ac28320e32afcc81351dd465824e94dd2ac4
Luke Kingland <lkingland2038@gmail.com>
2022-10-31 21:02:32 +0900

    fix: mock deployer method signature
    Updates the mock deployer to have the correct method signature
    (match interface it is mocking). Usage is illustrated using a
    refactored implementation of mock.NewDeployerWithResult.

--- mock/deployer.go
+++ mock/deployer.go
@@ -8,27 +8,25 @@ import (
 
 type Deployer struct {
 	DeployInvoked bool
-	DeployFn      func(fn.Function) error
-	DeployResult  *fn.DeploymentResult
+	DeployFn      func(context.Context, fn.Function) (fn.DeploymentResult, error)
 }
 
 func NewDeployer() *Deployer {
 	return &Deployer{
-		DeployFn: func(fn.Function) error { return nil },
-	}
-}
-
-func NewDeployerWithResult(result *fn.DeploymentResult) *Deployer {
-	return &Deployer{
-		DeployFn:     func(fn.Function) error { return nil },
-		DeployResult: result,
+		DeployFn: func(context.Context, fn.Function) (fn.DeploymentResult, error) { return fn.DeploymentResult{}, nil },
 	}
 }
 
 func (i *Deployer) Deploy(ctx context.Context, f fn.Function) (fn.DeploymentResult, error) {
 	i.DeployInvoked = true
-	if i.DeployResult != nil {
-		return *i.DeployResult, i.DeployFn(f)
+	return i.DeployFn(ctx, f)
+}
+
+// NewDeployerWithResult is a convenience method for creating a mock deployer
+// with a deploy function implementation which returns the given result
+// and no error.
+func NewDeployerWithResult(result fn.DeploymentResult) *Deployer {
+	return &Deployer{
+		DeployFn: func(context.Context, fn.Function) (fn.DeploymentResult, error) { return result, nil },
 	}
-	return fn.DeploymentResult{}, i.DeployFn(f)
 }
