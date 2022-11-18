399d2ab3a9f03950726e34e4e19a3ca4610fd3a1
Knative Prow Robot <knative-prow-robot@google.com>
2022-11-18 16:28:40 +0000

    [release-1.8] techdebt(buildpacks): test builder image prefix (#1441)
    * techdebt(buildpacks): test builder image prefix
    
    Adds a test for the known builder image prefixes, ensuring they are considered trusted. Removes the check for old versions of podman in order to facilitate this test without turning it into an integration test. Podman 3.4 was released in the summer of '21, more than a year ago, and all published versions of RHEL ship with podman 4.2.
    
    /kind techdebt
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    * fixup: remove unused code
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    * fixup: run ./hack/update-codegen.sh
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    * fixup: run ./hack/update-codegen.sh
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    Co-authored-by: Lance Ball <lball@redhat.com>

--- buildpacks/builder.go
+++ buildpacks/builder.go
@@ -8,7 +8,6 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/Masterminds/semver"
 	pack "github.com/buildpacks/pack/pkg/client"
 	"github.com/buildpacks/pack/pkg/logging"
 	"github.com/docker/docker/client"
@@ -38,8 +37,6 @@ var (
 		"gcr.io/paketo-buildpacks",
 		"docker.io/paketobuildpacks",
 	}
-
-	v330 = semver.MustParse("v3.3.0") // for checking podman version
 )
 
 // Builder will build Function using Pack.
@@ -121,6 +118,16 @@ func (b *Builder) Build(ctx context.Context, f fn.Function) (err error) {
 		opts.ContainerConfig.Network = "host"
 	}
 
+	// only trust our known builders
+	opts.TrustBuilder = func(_ string) bool {
+		for _, v := range trustedBuilderImagePrefixes {
+			if strings.HasPrefix(opts.Builder, v) {
+				return true
+			}
+		}
+		return false
+	}
+
 	var impl = b.impl
 	// Instantiate the pack build client implementation
 	// (and update build opts as necessary)
@@ -135,8 +142,10 @@ func (b *Builder) Build(ctx context.Context, f fn.Function) (err error) {
 			return fmt.Errorf("cannot craete docker client: %w", err)
 		}
 		defer cli.Close()
+		opts.DockerHost = dockerHost
 
-		if impl, err = newImpl(ctx, cli, dockerHost, &opts, b.logger); err != nil {
+		// Client with a logger which is enabled if in Verbose mode and a dockerClient that supports SSH docker daemon connection.
+		if impl, err = pack.NewClient(pack.WithLogger(b.logger), pack.WithDockerClient(cli)); err != nil {
 			return fmt.Errorf("cannot create pack client: %w", err)
 		}
 	}
@@ -155,54 +164,9 @@ func (b *Builder) Build(ctx context.Context, f fn.Function) (err error) {
 	return
 }
 
-// newImpl returns an instance of the builder implementation.  Note that this
-// also mutates the provided options' DockerHost and TrustBuilder.
-func newImpl(ctx context.Context, cli client.CommonAPIClient, dockerHost string, opts *pack.BuildOptions, logger logging.Logger) (impl Impl, err error) {
-	opts.DockerHost = dockerHost
-
-	daemonIsPodmanPreV330, err := podmanPreV330(ctx, cli)
-	if err != nil {
-		return
-	}
-
-	opts.TrustBuilder = func(_ string) bool {
-		if daemonIsPodmanPreV330 {
-			return false
-		}
-		for _, v := range trustedBuilderImagePrefixes {
-			if strings.HasPrefix(opts.Builder, v) {
-				return true
-			}
-		}
-		return false
-	}
-
-	// Client with a logger which is enabled if in Verbose mode and a dockerClient that supports SSH docker daemon connection.
-	return pack.NewClient(pack.WithLogger(logger), pack.WithDockerClient(cli))
-}
-
 // Builder Image chooses the correct builder image or defaults.
 func BuilderImage(f fn.Function, builderName string) (string, error) {
 	return builders.Image(f, builderName, DefaultBuilderImages)
-}
-
-// podmanPreV330 returns if the daemon is podman pre v330 or errors trying.
-func podmanPreV330(ctx context.Context, cli client.CommonAPIClient) (b bool, err error) {
-	version, err := cli.ServerVersion(ctx)
-	if err != nil {
-		return
-	}
-
-	for _, component := range version.Components {
-		if component.Name == "Podman Engine" {
-			v := semver.MustParse(version.Version)
-			if v.Compare(v330) < 0 {
-				return true, nil
-			}
-			break
-		}
-	}
-	return
 }
 
 // Errors
