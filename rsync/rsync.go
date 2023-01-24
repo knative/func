877da9f16d433a11c416e850625c17b8186dc5aa
Matej Vasek <mvasek@redhat.com>
2023-01-24 20:41:58 +0100

    fixup: add frysnc versioning
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- rsync/rsync.go
+++ rsync/rsync.go
@@ -1,7 +1,9 @@
 package rsync
 
 import (
+	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 	"io/fs"
 
@@ -13,10 +15,60 @@ var RemoteError = errors.New("remote error")
 type ProcessFile = func(path, wpath string, fi fs.FileInfo, err error) error
 type ForEachFile = func(processFile ProcessFile) error
 
+const latestVersion = uint16(1)
+
 func ReceiveFiles(conn io.ReadWriteCloser, root string) error {
-	return v1.ReceiveFiles(conn, root)
+	var buff [10]byte
+	_, err := conn.Read(buff[:])
+	if err != nil {
+		return fmt.Errorf("cannot read preamble: %w", err)
+	}
+	if string(buff[:8]) != "funcsync" {
+		return fmt.Errorf("invalid preamble")
+	}
+
+	ver := binary.BigEndian.Uint16(buff[8:])
+	if ver > latestVersion {
+		binary.BigEndian.PutUint16(buff[8:], latestVersion)
+		ver = latestVersion
+	}
+
+	_, err = conn.Write(buff[:])
+	if err != nil {
+		return fmt.Errorf("cannot reply to preamble: %w", err)
+	}
+
+	switch ver {
+	case 1:
+		return v1.ReceiveFiles(conn, root)
+	default:
+		return fmt.Errorf("unsupported protocol version")
+	}
 }
 
 func SendFiles(conn io.ReadWriteCloser, forEachFile ForEachFile) error {
-	return v1.SendFiles(conn, forEachFile)
+	var buff = []byte("funcsyncXX")
+	binary.BigEndian.PutUint16(buff[8:], latestVersion)
+
+	_, err := conn.Write(buff)
+	if err != nil {
+		return fmt.Errorf("cannot write preamble: %w", err)
+	}
+
+	_, err = conn.Read(buff[:])
+	if err != nil {
+		return fmt.Errorf("cannot read preamble: %w", err)
+	}
+	if string(buff[:8]) != "funcsync" {
+		return fmt.Errorf("invalid preamble response")
+	}
+
+	ver := binary.BigEndian.Uint16(buff[8:])
+	switch ver {
+	case 1:
+		return v1.SendFiles(conn, forEachFile)
+	default:
+		return fmt.Errorf("unsupported protocol version")
+	}
+
 }
