15fb9dc480e2507208a5cbe5ab4a8b27c7fa01c8
Matej Vasek <mvasek@redhat.com>
2023-03-02 19:36:46 +0100

    fixup: linter unused param
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- test/e2e/update_templates/typescript/http/src/index.ts
+++ test/e2e/update_templates/typescript/http/src/index.ts
@@ -1,5 +1,5 @@
 import { Context, Invokable } from 'faas-js-runtime';
 
-export const handle: Invokable = function (context: Context): string {
+export const handle: Invokable = function (_: Context): string {
     return 'HELLO TYPESCRIPT FUNCTION';
 };
