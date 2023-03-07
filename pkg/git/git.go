zroubalik/kn-plugin-func

feat: Pipelines as Code - initial support (pack)
Signed-off-by: Zbynek Roubalik <zroubalik@gmail.com>

pkg/git/git.go
@@ -1,0 +1,28 @@
+package git
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/formatting"
+)
+
+// GetRepoOwnerAndNameFromUrl for input url returns repo owner and repo name
+// eg. for github.com/foo/bar returns 'foo' and 'bar'
+func GetRepoOwnerAndNameFromUrl(url string) (string, string, error) {
+
+	defaultRepo, err := formatting.GetRepoOwnerFromURL(url)
+	if err != nil {
+		return "", "", err
+	}
+
+	defaultRepo = strings.TrimSuffix(defaultRepo, "/")
+	repoArr := strings.Split(defaultRepo, "/")
+	if len(repoArr) != 2 {
+		return "", "", fmt.Errorf("invalid repository, needs to be of format 'org-name/repo-name'")
+	}
+	repoOwner := repoArr[0]
+	repoName := repoArr[1]
+
+	return repoOwner, repoName, nil
+}

pkg/git/git_test.go
@@ -1,0 +1,48 @@
+package git
+
+import "testing"
+
+func TestGetRepoOwnerFromGHURL(t *testing.T) {
+	tests := []struct {
+		name      string
+		url       string
+		wantOwner string
+		wantName  string
+		wantErr   bool
+	}{
+		{
+			name:      "correct",
+			url:       "https://gh/foo/bar",
+			wantOwner: "foo",
+			wantName:  "bar",
+			wantErr:   false,
+		},
+		{
+			name:      "correct with capital letters",
+			url:       "https://gh/FOO/bar",
+			wantOwner: "foo",
+			wantName:  "bar",
+			wantErr:   false,
+		},
+		{
+			name:    "incorrect url",
+			url:     "foobar",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOwner, gotName, err := GetRepoOwnerAndNameFromUrl(tt.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetRepoOwnerAndNameFromUrl() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOwner != tt.wantOwner {
+				t.Errorf("GetRepoOwnerAndNameFromUrl() gotOwner = %v, wantOwner %v", gotOwner, tt.wantOwner)
+			}
+			if gotName != tt.wantName {
+				t.Errorf("GetRepoOwnerAndNameFromUrl() gotName = %v, wantName %v", gotName, tt.wantName)
+			}
+		})
+	}
+}

knative/func

pkg/git/git.go
@@ -1,0 +1,28 @@
+package git
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/openshift-pipelines/pipelines-as-code/pkg/formatting"
+)
+
+// RepoOwnerAndNameFromUrl for input url returns repo owner and repo name
+// eg. for github.com/foo/bar returns 'foo' and 'bar'
+func RepoOwnerAndNameFromUrl(url string) (string, string, error) {
+
+	defaultRepo, err := formatting.GetRepoOwnerFromURL(url)
+	if err != nil {
+		return "", "", err
+	}
+
+	defaultRepo = strings.TrimSuffix(defaultRepo, "/")
+	repoArr := strings.Split(defaultRepo, "/")
+	if len(repoArr) != 2 {
+		return "", "", fmt.Errorf("invalid repository, needs to be of format 'org-name/repo-name'")
+	}
+	repoOwner := repoArr[0]
+	repoName := repoArr[1]
+
+	return repoOwner, repoName, nil
+}

pkg/git/git_test.go
@@ -1,0 +1,48 @@
+package git
+
+import "testing"
+
+func TestGetRepoOwnerFromGHURL(t *testing.T) {
+	tests := []struct {
+		name      string
+		url       string
+		wantOwner string
+		wantName  string
+		wantErr   bool
+	}{
+		{
+			name:      "correct",
+			url:       "https://gh/foo/bar",
+			wantOwner: "foo",
+			wantName:  "bar",
+			wantErr:   false,
+		},
+		{
+			name:      "correct with capital letters",
+			url:       "https://gh/FOO/bar",
+			wantOwner: "foo",
+			wantName:  "bar",
+			wantErr:   false,
+		},
+		{
+			name:    "incorrect url",
+			url:     "foobar",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOwner, gotName, err := RepoOwnerAndNameFromUrl(tt.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetRepoOwnerAndNameFromUrl() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOwner != tt.wantOwner {
+				t.Errorf("GetRepoOwnerAndNameFromUrl() gotOwner = %v, wantOwner %v", gotOwner, tt.wantOwner)
+			}
+			if gotName != tt.wantName {
+				t.Errorf("GetRepoOwnerAndNameFromUrl() gotName = %v, wantName %v", gotName, tt.wantName)
+			}
+		})
+	}
+}