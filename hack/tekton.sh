d578c4dffb463e4cd7e2f8489c70a35a17f1fadb
Matej Vasek <mvasek@redhat.com>
2023-04-05 16:30:02 +0200

    fixup: shell-check
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- hack/tekton.sh
+++ hack/tekton.sh
@@ -25,26 +25,26 @@ export TERM="${TERM:-dumb}"
 tekton_release="previous/v0.42.0"
 git_clone_release="0.4"
 namespace="${NAMESPACE:-default}"
-tasks_source_path=$(dirname $(cd $(dirname $0) && pwd ))
+tasks_source_path="$(dirname "$(cd "$(dirname "$0")" && pwd )")"
 
 tekton() {
   echo "Installing Tekton..."
-  kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/${tekton_release}/release.yaml
+  kubectl apply -f "https://storage.googleapis.com/tekton-releases/pipeline/${tekton_release}/release.yaml"
   sleep 10
   kubectl wait pod --for=condition=Ready --timeout=180s -n tekton-pipelines -l "app=tekton-pipelines-controller"
   kubectl wait pod --for=condition=Ready --timeout=180s -n tekton-pipelines -l "app=tekton-pipelines-webhook"
   sleep 10
 
-  kubectl create clusterrolebinding ${namespace}:knative-serving-namespaced-admin \
-  --clusterrole=knative-serving-namespaced-admin  --serviceaccount=${namespace}:default
+  kubectl create clusterrolebinding "${namespace}:knative-serving-namespaced-admin" \
+  --clusterrole=knative-serving-namespaced-admin  --serviceaccount="${namespace}:default"
 }
 
 tekton_tasks() {
   echo "Creating Pipeline tasks..."
-  kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/${git_clone_release}/git-clone.yaml
-  kubectl apply -f ${tasks_source_path}/pkg/pipelines/resources/tekton/task/func-buildpacks/0.1/func-buildpacks.yaml
-  kubectl apply -f ${tasks_source_path}/pkg/pipelines/resources/tekton/task/func-s2i/0.1/func-s2i.yaml
-  kubectl apply -f ${tasks_source_path}/pkg/pipelines/resources/tekton/task/func-deploy/0.1/func-deploy.yaml
+  kubectl apply -f "https://raw.githubusercontent.com/tektoncd/catalog/master/task/git-clone/${git_clone_release}/git-clone.yaml"
+  kubectl apply -f "${tasks_source_path}/pkg/pipelines/resources/tekton/task/func-buildpacks/0.1/func-buildpacks.yaml"
+  kubectl apply -f "${tasks_source_path}/pkg/pipelines/resources/tekton/task/func-s2i/0.1/func-s2i.yaml"
+  kubectl apply -f "${tasks_source_path}/pkg/pipelines/resources/tekton/task/func-deploy/0.1/func-deploy.yaml"
 }
 
 ## Parse input parameters
@@ -56,7 +56,7 @@ if [ $# -gt 1 ] ; then
   exit 1
 fi
 if [ $# -eq 1 ] ; then
-    if [ $1 == "--tasks-only" ]
+    if [ "$1" == "--tasks-only" ]
       then
         tasks_only=true
     else
