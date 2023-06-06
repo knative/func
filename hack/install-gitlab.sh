5568d9a9374d442406f0ab7575b6cddf67727749
Matej Vasek <mvasek@redhat.com>
2023-06-06 17:54:24 +0200

    chore: clean up PaC GitLab test
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- hack/install-gitlab.sh
+++ hack/install-gitlab.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function install_gitlab() {
-  local -r gitlab_host="gitlab.127.0.0.1.sslip.io"
+  local -r gitlab_host="${GITLAB_HOSTNAME:-gitlab.127.0.0.1.sslip.io}"
 
   kubectl apply -f - <<EOF
 kind: Namespace
@@ -155,6 +155,7 @@ EOF
     return 1
   fi
   echo '::endgroup::'
+  echo "the GitLab server is available at: http://${gitlab_host}"
 }
 
 if [ "$0" = "${BASH_SOURCE[0]}" ]; then
