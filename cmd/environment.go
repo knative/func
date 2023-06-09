1c4e11cbc59ae8d5e18816b70ee3a8b1b83fed96
ntishchauhan0022 <nitishchauhan0022@gmail.com>
2023-06-09 11:16:38 +0530

    adding yaml format output option
    Signed-off-by: ntishchauhan0022 <nitishchauhan0022@gmail.com>

--- cmd/environment.go
+++ cmd/environment.go
@@ -8,12 +8,16 @@ import (
 
 	"github.com/ory/viper"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 
+	"knative.dev/func/pkg/builders/buildpacks"
 	"knative.dev/func/pkg/builders/s2i"
 	"knative.dev/func/pkg/config"
 	"knative.dev/func/pkg/functions"
 	"knative.dev/func/pkg/k8s"
 )
+
+var EnvFormat string = "json"
 
 func NewEnvironmentCmd(newClient ClientFactory, version *Version) *cobra.Command {
 	cmd := &cobra.Command{
@@ -21,10 +25,11 @@ func NewEnvironmentCmd(newClient ClientFactory, version *Version) *cobra.Command
 		Short: "Display function execution environment information",
 		Long: `
 NAME
-	{{rootCmdUse}} environment
+	{{rootCmdUse}} environment - display function execution environment information
 
 SYNOPSIS
-	{{rootCmdUse}} environment - display function execution environment information
+	{{rootCmdUse}} environment [-e|--env-format] [-v|--verbose]
+
 
 DESCRIPTION
 	Display information about the function execution environment, including
@@ -32,6 +37,7 @@ DESCRIPTION
 	available runtimes, and available templates.
 `,
 		SuggestFor: []string{"env", "environemtn", "enviroment", "enviornment", "enviroment"},
+		PreRunE:    bindEnv("verbose", "env-format"),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runEnvironment(cmd, newClient, version)
 		},
@@ -41,23 +47,24 @@ DESCRIPTION
 		fmt.Fprintf(cmd.OutOrStdout(), "error loading config at '%v'. %v\n", config.File(), err)
 	}
 
+	cmd.Flags().StringP("env-format", "e", EnvFormat, "Format of output environment informatio, 'json' or 'yaml'. ($FUNC_ENV_FORMAT)")
 	addVerboseFlag(cmd, cfg.Verbose)
 
 	return cmd
 }
 
 type Environment struct {
-	Version         string
-	GitRevision     string
-	SpecVersion     string
-	SocatImage      string
-	TarImage        string
-	Languages       []string
-	DefaultBuilders map[string]string
-	Templates       map[string][]string
-	Environment     []string
-	Cluster         string
-	Defaults        config.Global
+	Version              string
+	GitRevision          string
+	SpecVersion          string
+	SocatImage           string
+	TarImage             string
+	Languages            []string
+	DefaultImageBuilders map[string]map[string]string
+	Templates            map[string][]string
+	Environment          []string
+	Cluster              string
+	Defaults             config.Global
 }
 
 func runEnvironment(cmd *cobra.Command, newClient ClientFactory, v *Version) (err error) {
@@ -107,25 +114,38 @@ func runEnvironment(cmd *cobra.Command, newClient ClientFactory, v *Version) (er
 		host = cc.Host
 	}
 
+	//Get default image builders
+	builderimagesdefault := make(map[string]map[string]string)
+	builderimagesdefault["s2i"] = s2i.DefaultBuilderImages
+	builderimagesdefault["buildpacks"] = buildpacks.DefaultBuilderImages
+
 	environment := Environment{
-		Version:         v.String(),
-		GitRevision:     v.Hash,
-		SpecVersion:     functions.LastSpecVersion(),
-		SocatImage:      k8s.SocatImage,
-		TarImage:        k8s.TarImage,
-		Languages:       r,
-		DefaultBuilders: s2i.DefaultBuilderImages,
-		Templates:       t,
-		Environment:     envs,
-		Cluster:         host,
-		Defaults:        defaults,
+		Version:              v.String(),
+		GitRevision:          v.Hash,
+		SpecVersion:          functions.LastSpecVersion(),
+		SocatImage:           k8s.SocatImage,
+		TarImage:             k8s.TarImage,
+		Languages:            r,
+		DefaultImageBuilders: builderimagesdefault,
+		Templates:            t,
+		Environment:          envs,
+		Cluster:              host,
+		Defaults:             defaults,
 	}
 
-	if s, err := json.MarshalIndent(environment, "", "  "); err != nil {
-		return err
-	} else {
-		fmt.Fprintln(cmd.OutOrStdout(), string(s))
+	var s []byte
+	switch cfg.Format {
+	case "json":
+		s, err = json.MarshalIndent(environment, "", "  ")
+	case "yaml":
+		s, err = yaml.Marshal(&environment)
+	default:
+		err = fmt.Errorf("unsupported format: %s", cfg.Format)
 	}
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(cmd.OutOrStdout(), string(s))
 
 	return nil
 }
@@ -152,12 +172,13 @@ func getTemplates(client *functions.Client, runtimes []string) (map[string][]str
 
 type environmentConfig struct {
 	Verbose bool
-	// TODO: add format (e.g. JSON/YAML)
+	Format  string
 }
 
 func newEnvironmentConfig() (cfg environmentConfig, err error) {
 	cfg = environmentConfig{
 		Verbose: viper.GetBool("verbose"),
+		Format:  viper.GetString("env-format"),
 	}
 
 	return
