7018d61ab90f1548038796d994c73aa8b511fe61
Luke Kingland <luke@lukekingland.com>
2023-06-16 13:55:43 +0900

    src: make test and check go
    - Updates make test-go to include scaffolding
    - Creates make check-go to run basic source quality checks

--- templates/go/scaffolding/static-cloudevents/main.go
+++ templates/go/scaffolding/static-cloudevents/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	if err := ce.Start(ce.DefaultHandler{f.Handle}); err != nil {
+	if err := ce.Start(ce.DefaultHandler{Handler: f.Handle}); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
