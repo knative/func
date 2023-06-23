f939f22b243d71c7a4d25f21e14cda1af17d53ec
Luke Kingland <luke@lukekingland.com>
2023-06-23 19:56:27 +0900

    feat: host builder certs layer

--- pkg/filesystem/filesystem_test.go
+++ pkg/filesystem/filesystem_test.go
@@ -158,7 +158,7 @@ func loadLocalFiles(root string) ([]FileInfo, error) {
 			}
 		case fs.ModeSymlink:
 			t, _ := os.Readlink(path)
-			bs = []byte(t)
+			bs = []byte(filepath.ToSlash(t))
 		}
 		path, err = filepath.Rel(root, path)
 		if err != nil {
