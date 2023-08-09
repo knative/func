0e23489f085c7efb3fef7718ae4f093130f06a73
Matej Vasek <mvasek@redhat.com>
2023-08-09 23:12:56 +0200

    fixup
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- pkg/pipelines/tekton/validate_test.go
+++ pkg/pipelines/tekton/validate_test.go
@@ -108,7 +108,7 @@ func Test_validatePipeline(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			_, err := validatePipeline(tt.function)
+			err := validatePipeline(tt.function)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("validatePipeline() error = %v, wantErr %v", err, tt.wantErr)
 				return
