df97bd79e76872a18224958dde08439ed84386fe
Matej Vasek <mvasek@redhat.com>
2023-09-14 17:42:42 +0200

    chore: use current func image in tests (#1974)
    Use a different approach to pass current func image to the tests, this
    approach does not require modification of task yaml nor access
    to outer internet (ttl.sh).
    
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- hack/create-testing-func-image.sh
+++ hack/create-testing-func-image.sh
@@ -4,12 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-KO_DOCKER_REPO="ttl.sh/$(head -c 128 </dev/urandom | LC_CTYPE=C tr -dc 'a-z0-9' | fold -w 8 | head -n 1)"
+KO_DOCKER_REPO="localhost:50000/knative/func"
 export KO_DOCKER_REPO
 
-REF_FILE=$(mktemp)
-
-ko build --image-refs "${REF_FILE}" --tags "30m" -B ./cmd/func
-
-yq -Y -i ".spec.steps[0].image = \"$(cat "${REF_FILE}")\"" \
-  pkg/pipelines/resources/tekton/task/func-deploy/0.1/func-deploy.yaml
+ko build --tags "latest" -B ./cmd/func
