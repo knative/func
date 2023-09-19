6047143d2096f98751205fca40d4500b0362a195
Jefferson Ramos <jeramos@redhat.com>
2023-09-19 08:46:18 -0300

    test: Split of GH oncluster tests by builder. Added FUNC_BUILDER env var for e2e oncluter tests (#1963)

--- test/oncluster/scenario_runtime_test.go
+++ test/oncluster/scenario_runtime_test.go
@@ -28,6 +28,7 @@ func TestRuntime(t *testing.T) {
 
 	var runtimeList = []string{}
 	runtimes, present := os.LookupEnv("E2E_RUNTIMES")
+	targetBuilder, _ := os.LookupEnv("FUNC_BUILDER")
 
 	if present {
 		if runtimes != "" {
@@ -41,9 +42,11 @@ func TestRuntime(t *testing.T) {
 
 	for _, lang := range runtimeList {
 		for _, builder := range runtimeSupportMap[lang] {
-			t.Run(fmt.Sprintf("%v_%v_test", lang, builder), func(t *testing.T) {
-				runtimeImpl(t, lang, builder)
-			})
+			if targetBuilder == "" || builder == targetBuilder {
+				t.Run(fmt.Sprintf("%v_%v_test", lang, builder), func(t *testing.T) {
+					runtimeImpl(t, lang, builder)
+				})
+			}
 		}
 	}
 
