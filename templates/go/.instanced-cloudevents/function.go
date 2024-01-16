9ae3e50834b2d1d50680ff22b0c65d07eb58606a
Matthias Wessendorf <mwessend@redhat.com>
2024-01-16 12:00:47 +0100

    add lifecycles
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- templates/go/.instanced-cloudevents/function.go
+++ templates/go/.instanced-cloudevents/function.go
@@ -56,10 +56,44 @@ func (f *MyFunction) Handle(ctx context.Context, e event.Event) (*event.Event, e
 	return &e, nil // echo to caller
 }
 
-// TODO: Start
-// TODO: Stop
-// TODO: Alive
-// TODO: Ready
+// Start is called whenever a function instance is started.
+//
+// Provided to this start method are all arguments and environment variables
+// which apply to this function.  For better function portability, testability
+// and robustness, it is encouraged to use this method for accessing function
+// configuration rather than looking for environment variables or flags.
+// func (f *MyFunction) Start(ctx context.Context, args map[string]string) error {
+//   fmt.Println("Function Started")
+//   return nil
+// }
+
+// Stop is called whenever a function is stopped.
+//
+// This may happen for reasons such as being rescheduled onto a different node,
+// being updated with a newer version, or if the number of function instances
+// is being scaled down due to low load.  This is a good place to cleanup and
+// realease any resources which expect to be manually released.
+//
+// func (f *Function) Stop(ctx context.Context) error { return nil }
+
+// Alive is an optional method which allows you to more deeply indicate that
+// your function is alive.  The default liveness implementation returns true
+// if the function process is not deadlocked and able to respond.  A custom
+// implementation of this method may be useful when a function should not be
+// considered alive if any dependent services are alive, or other more
+// complex logic.
+//
+// func (f *Function) Alive(ctx context.Context) (bool, error) {
+//   return true, nil
+// }
+
+// Ready is an optional method which, when implemented, will ensure that
+// requests are not made to the Function's request handler until this method
+// reports true.
+//
+// func (f *Function) Ready(ctx context.Context) (bool, error) {
+//   return true, nil
+// }
 
 // Handle is an optional method which can be used to implement simple
 // functions with little or no state and minimal testing requirements.  By
