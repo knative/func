lkingland/kn-plugin-func

feat: host builder basic auth

pkg/oci/pusher.go
@@ -3,6 +3,7 @@ package oci
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -11,29 +12,27 @@ import (
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/google"
 	"github.com/google/go-containerregistry/pkg/v1/layout"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/pkg/errors"
 	progress "github.com/schollz/progressbar/v3"
+
 	fn "knative.dev/func/pkg/functions"
 )
 
 // Pusher of OCI multi-arch layout directories.
 type Pusher struct {
-	Insecure bool
-	Verbose  bool
-	Username string
-	Token    string
-
+	Verbose bool
 	updates chan v1.Update
 	done    chan bool
 }
 
-func NewPusher(insecure, verbose bool) *Pusher {
+func NewPusher(verbose bool) *Pusher {
 	return &Pusher{
-		Insecure: insecure,
-		Verbose:  verbose,
-		updates:  make(chan v1.Update, 10),
-		done:     make(chan bool, 1),
+		Verbose: verbose,
+		updates: make(chan v1.Update, 10),
+		done:    make(chan bool, 1),
 	}
 }
 
@@ -69,43 +68,6 @@ func (p *Pusher) Push(ctx context.Context, f fn.Function) (digest string, err er
 	return
 }
 
-// The last build directory is symlinked upon successful build.
-func getLastBuildDir(f fn.Function) (string, error) {
-	dir := filepath.Join(f.Root, fn.RunDataDir, "builds", "last")
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		return dir, fmt.Errorf("last build directory not found '%v'. Has it been built?", dir)
-	}
-	return dir, nil
-}
-
-func (p *Pusher) writeIndex(ctx context.Context, ref name.Reference, ii v1.ImageIndex) error {
-	// If we're set to insecure, just try as-is and return on failure
-	if p.Insecure {
-		return remote.WriteIndex(ref, ii,
-			remote.WithContext(ctx),
-			remote.WithProgress(p.updates))
-	}
-
-	// TODO: The below is untested and may only be useful for the test, since
-	// a `docker login` utilizes the keychain for the httpasswd basic auth
-	// credentials as well:
-	if p.Username != "" && p.Token != "" {
-		return remote.WriteIndex(ref, ii,
-			remote.WithContext(ctx),
-			remote.WithProgress(p.updates),
-			remote.WithAuth(&authn.Basic{
-				Username: p.Username,
-				Password: p.Token,
-			}))
-	}
-
-	// Otherwise use the keychain
-	return remote.WriteIndex(ref, ii,
-		remote.WithContext(ctx),
-		remote.WithProgress(p.updates),
-		remote.WithAuthFromKeychain(authn.DefaultKeychain))
-}
-
 func (p *Pusher) handleUpdates(ctx context.Context) {
 	var bar *progress.ProgressBar
 	for {
@@ -133,5 +95,79 @@ func (p *Pusher) handleUpdates(ctx context.Context) {
 			return
 		}
 	}
+}
 
+// getLastBuildDir is a convenience method for deriving the path to the
+// directory used for the last build.
+// {f.root}/.func/builds/last
+func getLastBuildDir(f fn.Function) (string, error) {
+	dir := filepath.Join(f.Root, fn.RunDataDir, "builds", "last")
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return dir, fmt.Errorf("last build directory not found '%v'. Has it been built?", dir)
+	}
+	return dir, nil
+}
+
+// writeIndex to its defined registry.
+func (p *Pusher) writeIndex(ctx context.Context, ref name.Reference, ii v1.ImageIndex) error {
+
+	authOption, err := p.authOption(ctx, ref)
+	if err != nil {
+		return err
+	}
+
+	transport := remote.DefaultTransport.(*http.Transport)
+	// if p.insecureSkipVerify {
+	// 	transport = transport.Clone()
+	// 	transport.TLSClientConfig = &tls.Config{
+	// 		InsecureSkipVerify: true,
+	// 	}
+	// }
+
+	return remote.WriteIndex(ref, ii,
+		remote.WithTransport(transport),
+		remote.WithContext(ctx),
+		remote.WithProgress(p.updates),
+		authOption)
+
+	// TODO: anonymous auth
+	// We could try anonymously here, or perhaps have anon as a fallback
+	// within the MultiKeychain?
+	// return remote.WriteIndex(ref, ii,
+	// 	 remote.WithContext(ctx),
+	// 	 remote.WithProgress(p.updates))
+
+}
+
+// authOption selects an appropriate authentication option.
+// If user provided = basic auth (secret is password)
+// If only secret provided = bearer token auth
+// If neither are provided = Returned is a cascading keychain auth mthod
+// which performs the following in order:
+// - Default Keychain (docker and podman config files)
+// - Google Keychain
+// - TODO: ECR Amazon
+// - TODO: ACR Azure
+func (p *Pusher) authOption(ctx context.Context, ref name.Reference) (remote.Option, error) {
+
+	// Basic Auth if provided
+	username, _ := ctx.Value(fn.PushUsernameKey{}).(string)
+	password, _ := ctx.Value(fn.PushPasswordKey{}).(string)
+	token, _ := ctx.Value(fn.PushTokenKey{}).(string)
+	if username != "" && token != "" {
+		return nil, errors.New("only one of username/password or token authentication allowed.  Received both a token and username")
+	} else if token != "" {
+		return remote.WithAuth(&authn.Bearer{Token: token}), nil
+	} else if username != "" {
+		return remote.WithAuth(&authn.Basic{Username: username, Password: password}), nil
+	}
+
+	// Default chain
+	return remote.WithAuthFromKeychain(authn.NewMultiKeychain(
+		authn.DefaultKeychain, // Podman and Docker config files
+		google.Keychain,       // Google
+		// TODO: Integrate and test ECR and ACR credential helpers:
+		// authn.NewKeychainFromHelper(ecr.ECRHelper{ClientFactory: api.DefaultClientFactory{}}),
+		// authn.NewKeychainFromHelper(acr.ACRCredHelper{}),
+	)), nil
 }

knative/func

pkg/oci/pusher.go
@@ -2,7 +2,9 @@ package oci
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -11,26 +13,28 @@ import (
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/google"
 	"github.com/google/go-containerregistry/pkg/v1/layout"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/pkg/errors"
 	progress "github.com/schollz/progressbar/v3"
+
 	fn "knative.dev/func/pkg/functions"
 )
 
 // Pusher of OCI multi-arch layout directories.
 type Pusher struct {
-	Insecure  bool
 	Anonymous bool
-	Verbose   bool
-	Username  string
+	Insecure  bool
 	Token     string
+	Username  string
+	Verbose   bool
 
 	updates chan v1.Update
 	done    chan bool
 }
 
 func NewPusher(insecure, anon, verbose bool) *Pusher {
-	fmt.Println(insecure, anon, verbose)
 	return &Pusher{
 		Insecure:  insecure,
 		Anonymous: anon,
@@ -77,43 +81,6 @@ func (p *Pusher) Push(ctx context.Context, f fn.Function) (digest string, err er
 	return
 }
 
-// The last build directory is symlinked upon successful build.
-func getLastBuildDir(f fn.Function) (string, error) {
-	dir := filepath.Join(f.Root, fn.RunDataDir, "builds", "last")
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		return dir, fmt.Errorf("last build directory not found '%v'. Has it been built?", dir)
-	}
-	return dir, nil
-}
-
-func (p *Pusher) writeIndex(ctx context.Context, ref name.Reference, ii v1.ImageIndex) error {
-	// If we're set to anonymous, just try as-is and return on failure
-	if p.Anonymous {
-		return remote.WriteIndex(ref, ii,
-			remote.WithContext(ctx),
-			remote.WithProgress(p.updates))
-	}
-
-	// TODO: The below is untested and may only be useful for the test, since
-	// a `docker login` utilizes the keychain for the httpasswd basic auth
-	// credentials as well:
-	if p.Username != "" && p.Token != "" {
-		return remote.WriteIndex(ref, ii,
-			remote.WithContext(ctx),
-			remote.WithProgress(p.updates),
-			remote.WithAuth(&authn.Basic{
-				Username: p.Username,
-				Password: p.Token,
-			}))
-	}
-
-	// Otherwise use the keychain
-	return remote.WriteIndex(ref, ii,
-		remote.WithContext(ctx),
-		remote.WithProgress(p.updates),
-		remote.WithAuthFromKeychain(authn.DefaultKeychain))
-}
-
 func (p *Pusher) handleUpdates(ctx context.Context) {
 	var bar *progress.ProgressBar
 	for {
@@ -141,5 +108,72 @@ func (p *Pusher) handleUpdates(ctx context.Context) {
 			return
 		}
 	}
+}
 
+// The last build directory is symlinked upon successful build.
+func getLastBuildDir(f fn.Function) (string, error) {
+	dir := filepath.Join(f.Root, fn.RunDataDir, "builds", "last")
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return dir, fmt.Errorf("last build directory not found '%v'. Has it been built?", dir)
+	}
+	return dir, nil
+}
+
+// writeIndex to its defined registry.
+func (p *Pusher) writeIndex(ctx context.Context, ref name.Reference, ii v1.ImageIndex) error {
+	oo := []remote.Option{
+		remote.WithContext(ctx),
+		remote.WithProgress(p.updates),
+	}
+
+	if p.Insecure {
+		t := remote.DefaultTransport.(*http.Transport).Clone()
+		t.TLSClientConfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+		oo = append(oo, remote.WithTransport(t))
+	}
+
+	if !p.Anonymous {
+		a, err := p.authOption(ctx, ref)
+		if err != nil {
+			return err
+		}
+		oo = append(oo, a)
+	}
+
+	return remote.WriteIndex(ref, ii, oo...)
+}
+
+// authOption selects an appropriate authentication option.
+// If user provided = basic auth (secret is password)
+// If only secret provided = bearer token auth
+// If neither are provided = Returned is a cascading keychain auth mthod
+// which performs the following in order:
+// - Default Keychain (docker and podman config files)
+// - Google Keychain
+// - TODO: ECR Amazon
+// - TODO: ACR Azure
+func (p *Pusher) authOption(ctx context.Context, ref name.Reference) (remote.Option, error) {
+
+	// Basic Auth if provided
+	username, _ := ctx.Value(fn.PushUsernameKey{}).(string)
+	password, _ := ctx.Value(fn.PushPasswordKey{}).(string)
+	token, _ := ctx.Value(fn.PushTokenKey{}).(string)
+	if username != "" && token != "" {
+		return nil, errors.New("only one of username/password or token authentication allowed.  Received both a token and username")
+	} else if token != "" {
+		return remote.WithAuth(&authn.Bearer{Token: token}), nil
+	} else if username != "" {
+		return remote.WithAuth(&authn.Basic{Username: username, Password: password}), nil
+	}
+
+	// Default chain
+	return remote.WithAuthFromKeychain(authn.NewMultiKeychain(
+		authn.DefaultKeychain, // Podman and Docker config files
+		google.Keychain,       // Google
+		// TODO: Integrate and test ECR and ACR credential helpers:
+		// authn.NewKeychainFromHelper(ecr.ECRHelper{ClientFactory: api.DefaultClientFactory{}}),
+		// authn.NewKeychainFromHelper(acr.ACRCredHelper{}),
+	)), nil
 }