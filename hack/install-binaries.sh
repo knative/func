e4010db028e72b90fbe745e1fae4b6dbd3ed08c8
Luke Kingland <luke@lukekingland.com>
2024-04-02 08:29:03 +0000

    src: cleanup setup scripts (#2247)
    The current binaries and allocation scrips are very CI-centric, this PR
    takes a few steps to make them more friendly to running in a dev
    environment; mainly centered around not polluting global scope (keeping
    filesystem mutations in a relative local directory).
    
    - binaries (and kind kubeconfig) are installed in ./hack/bin
    - adds /hack/bin to .gitignore
    - binary paths can be overridden via env vars FUNC_TEST_{name}
    - binary fallback is system-installed (when binaries.sh not run)
    - Cleanup of the UX, including colors and some shared helpers via
      library.sh
    - Removes `yq` as the hack that required it appears to no longer be necessary.
    - Adds some notes for Nix users
    - Better error messaging when !linux
    - Refactors the installers to pipe directly to tar rather than touch
      disk.
    - begin to standardize on hyphens in filenames

---
+++ hack/install-binaries.sh
@@ -0,0 +1,132 @@
+#//usr/bin/env bash
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# Installs binaries on linux systems.
+#
+
+source "$(dirname "$(realpath "$0")")/common.sh"
+
+install_binaries() {
+  assert_linux
+  warn_architecture
+
+  local root="$(dirname "$(realpath "$0")")"
+  local bin="${root}/bin"
+
+  local kubectl_version=1.29.2
+  local kind_version=0.22.0
+  local dapr_version=1.11.0
+  local helm_version=3.12.0
+  local stern_version=1.25.0
+  local kn_version=1.13.0
+  local jq_version=1.7.1
+
+  echo "${blue}Installing binaries${reset}"
+  echo "  Architecture: ${ARCH}"
+  echo "  Destination:  ${bin}"
+
+  mkdir -p "${bin}"
+
+  install_kubectl
+  install_kind
+  install_dapr
+  install_helm
+  install_stern
+  install_kn
+  install_jq
+
+  echo "${green}DONE${reset}"
+
+}
+
+assert_linux() {
+  os_name=$(uname -s)
+  if [ "$os_name" != "Linux" ]; then
+    echo "${yellow}----------------------------------------------------------------------${reset}"
+    echo "${yellow}This script currently only supports Linux${reset}"
+    echo "Please install the dependencies manually"
+    echo "${yellow}----------------------------------------------------------------------${reset}"
+    exit 1
+  fi
+}
+
+warn_architecture() {
+  arch=$(uname -m)
+  if [ "$arch" != "x86_64" ]; then
+    echo -e "${yellow}Detected untested architecture ${arch}.${reset}\n This script is only tested with amd64, but you can use the ARCH env variable to specify an architecture to be interpolated in download links."
+  fi
+}
+
+install_kubectl() {
+    echo '=== kubectl'
+    curl -sSLo "${bin}"/kubectl "https://storage.googleapis.com/kubernetes-release/release/v$kubectl_version/bin/linux/${ARCH}/kubectl"
+    chmod +x "${bin}"/kubectl
+    "${bin}"/kubectl version --client=true
+}
+
+install_kind() {
+    echo '=== kind'
+    curl -sSLo "${bin}"/kind "https://github.com/kubernetes-sigs/kind/releases/download/v$kind_version/kind-linux-${ARCH}"
+    chmod +x "${bin}"/kind
+    "${bin}"/kind version
+}
+
+install_dapr() {
+    echo '=== dapr'
+    curl -sSL "https://github.com/dapr/cli/releases/download/v$dapr_version/dapr_linux_${ARCH}.tar.gz" | \
+      tar fxz - -C "${bin}" dapr
+    "${bin}"/dapr version
+}
+
+install_helm() {
+  echo '=== helm'
+    curl -sSL "https://get.helm.sh/helm-v$helm_version-linux-${ARCH}.tar.gz" | \
+      tar fxz - -C "${bin}" linux-"${ARCH}"/helm
+    mv "${bin}/linux-${ARCH}"/helm "${bin}" && rmdir "${bin}/linux-${ARCH}"
+    "${bin}"/helm version
+}
+
+install_stern() {
+  echo '=== stern'
+  curl -sSL "https://github.com/stern/stern/releases/download/v${stern_version}/stern_${stern_version}_linux_${ARCH}.tar.gz" | \
+    tar fxz - -C "${bin}" stern
+  "${bin}"/stern -v
+}
+
+install_kn() {
+  echo '=== kn'
+  curl -sSLo "${bin}"/kn "https://github.com/knative/client/releases/download/knative-v${kn_version}/kn-linux-${ARCH}"
+  chmod +x "${bin}"/kn
+  "${bin}"/kn version
+}
+
+install_jq() {
+  echo '=== jq'
+                       # "https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64"
+  curl -sSLo "${bin}"/jq "https://github.com/jqlang/jq/releases/download/jq-${jq_version}/jq-linux-${ARCH}"
+  chmod +x "${bin}"/jq
+  "${bin}"/jq --version
+}
+
+if [ "$0" = "${BASH_SOURCE[0]}" ]; then
+  set -o errexit
+  set -o nounset
+  set -o pipefail
+
+  function main() {
+    install_binaries
+  }
+  main "$@"
+fi
--- hack/registry.sh
+++ hack/registry.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # 
 # - Registers registry with Docker as trusted (linux only)
 #
@@ -8,15 +20,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CONTAINER_ENGINE=${CONTAINER_ENGINE:-docker}
-export TERM="${TERM:-dumb}"
+source "$(dirname "$(realpath "$0")")/common.sh"
 
-main() {
-  local em=$(tput bold)$(tput setaf 2)
-  local me=$(tput sgr0)
+registry() {
+  echo "${blue}Enabling Insecure Local Registry${reset}"
 
-  echo "${em}Configuring for CI...${me}"
-
+  warn_nix
 
   # Check the value of CONTAINER_ENGINE
   echo 'Setting registry as trusted local-only'
@@ -26,8 +35,25 @@ main() {
       set_registry_insecure_podman
   fi
 
-  echo "${em}DONE${me}"
+  echo "${green}✅ Registry${reset}"
+}
 
+warn_nix() {
+    if [[ -x $(command -v "nix") || -x $(command -v "nixos-rebuild") ]]; then
+        if [ "$CONTAINER_ENGINE" == "docker" ]; then
+          echo "${yellow}Warning: Nix detected${reset}"
+          echo "If Docker was configured using nix, this command will fail to find daemon.json. please configure the insecure registry by modifying your nix config:"
+          echo "  virtualisation.docker = {"
+          echo "    enable = true;"
+          echo "    daemon.settings.insecure-registries = [ \"localhost:50000\" ];"
+          echo "  };"
+        elif [ "$CONTAINER_ENGINE" == "podman" ]; then
+          echo "${yellow}Warning: Nix detected${reset}"
+          echo "If podman was configured via Nix, this command will likely fail.  At time of this writing, podman configured via the nix option 'virtualisation.podman' does not have an option for configuring insecure registries."
+          echo "The configuration required is adding the following to registries.conf:"
+          echo -e "  [[registry-insecure-local]]\n  location = \"localhost:50000\"\n  insecure = true"
+        fi
+    fi
 }
 
 set_registry_insecure() {
@@ -46,5 +72,13 @@ set_registry_insecure_podman() {
     fi
 }
 
-main "$@"
+if [ "$0" = "${BASH_SOURCE[0]}" ]; then
+  set -o errexit
+  set -o nounset
+  set -o pipefail
 
+  function main() {
+    registry
+  }
+  main "$@"
+fi
