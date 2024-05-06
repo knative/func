f6ffd416655286db8e422bdacbb7442b9c5026d8
Luke Kingland <luke@lukekingland.com>
2024-05-06 15:43:20 +0900

    add interactive E2E stubs

---
+++ e2e/testdata/e2e_test_interactive.go
@@ -0,0 +1,40 @@
+package e2e
+
+import (
+	"testing"
+)
+
+// TestInteractive_Init ensures that `func init` can be completed
+// interactively.
+func TestInteractive_Init(t *testing.T) {
+	t.Log("Not Implemented")
+}
+
+// TestInteractive_Deploy ensures that `func deploy` can be completed
+// interactively.
+func TestInteractive_Deploy(t *testing.T) {
+	t.Log("Not Implemented")
+}
+
+// TestInteractive_Events ensures that event metadata can be mutated
+// interactively.
+func TestInteractive_Events(t *testing.T) {
+	t.Log("Not Implemented")
+}
+
+// TestInteractive_Volumes ensures that volumes can be mutated interactively.
+func TestInteractive_Volumes(t *testing.T) {
+	t.Log("Not Implemented")
+}
+
+// TestInteractive_Envs ensures that environment variables can be mutated
+// interactively.
+func TestInteractive_Envs(t *testing.T) {
+	t.Log("Not Implemented")
+}
+
+// TestInteractive_Labels ensures that labels (metadata) can be mutated
+// interactively.
+func TestInteractive_Labels(t *testing.T) {
+	t.Log("Not Implemented")
+}
