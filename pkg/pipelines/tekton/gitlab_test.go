feb06f52b0d328ac2eb7c3bb51bf30b029a79596
Matej Vašek <mvasek@redhat.com>
2024-05-22 17:19:36 +0200

    Fix PaC build (#2314)
    * fix: default NS for PaC
    
    Signed-off-by: Matej Vašek <mvasek@redhat.com>
    
    * fix: make error non-fatal
    
    Signed-off-by: Matej Vašek <mvasek@redhat.com>
    
    * fix: pass correct image to PaC
    
    Signed-off-by: Matej Vašek <mvasek@redhat.com>
    
    * chore: enable gitlab test
    
    ---------
    
    Signed-off-by: Matej Vašek <mvasek@redhat.com>

--- pkg/pipelines/tekton/gitlab_test.go
+++ pkg/pipelines/tekton/gitlab_test.go
@@ -44,10 +44,6 @@ import (
 )
 
 func TestGitlab(t *testing.T) {
-	// this test is unreliable and fails majority of the time with unrelated problem
-	// skip this for now until the issue is fixed
-	// https://github.com/knative/func/issues/2185
-	t.Skip()
 	var err error
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
@@ -237,7 +233,8 @@ func setupGitlabEnv(ctx context.Context, t *testing.T, baseURL, username, passwo
 	}
 	_, _, err = glabCli.Settings.UpdateSettings(newSettings)
 	if err != nil {
-		t.Fatal(err)
+		// just log the error, it appears that despite the error the setting is updated
+		t.Log(err)
 	}
 	// For some reason the setting update does not kick in immediately.
 
