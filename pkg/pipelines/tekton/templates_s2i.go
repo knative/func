7f2ee94f9253ce85c63f04b7a3048d2429f279c2
Matej Vašek <mvasek@redhat.com>
2024-08-27 14:51:38 +0200

    Updated git-clone task

--- pkg/pipelines/tekton/templates_s2i.go
+++ pkg/pipelines/tekton/templates_s2i.go
@@ -118,6 +118,9 @@ metadata:
     {{end}}
   generateName: {{.PipelineRunName}}
 spec:
+  podTemplate:
+    securityContext:
+      fsGroup: 65532
   params:
     - name: gitRepository
       value: {{.RepoUrl}}
