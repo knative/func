be3a8a07b624f5c9294686c4366b17eb5f2feeeb
Jefferson Ramos <jeramos@redhat.com>
2024-10-01 14:21:21 -0300

    fix: Remove unnecesary annotations from Pipeline-as-Code template (#2527)

--- pkg/pipelines/tekton/templates_s2i.go
+++ pkg/pipelines/tekton/templates_s2i.go
@@ -174,12 +174,6 @@ metadata:
     # Fetch the git-clone task from hub
     pipelinesascode.tekton.dev/task: {{.GitCloneTaskRef}}
 
-    # Fetch the func-s2i task
-    pipelinesascode.tekton.dev/task-1: {{.FuncS2iTaskRef}}
-
-    # Fetch the func-deploy task
-    pipelinesascode.tekton.dev/task-2: {{.FuncDeployTaskRef}}
-
     # Fetch the pipelie definition from the .tekton directory
     pipelinesascode.tekton.dev/pipeline: {{.PipelineYamlURL}}
 
