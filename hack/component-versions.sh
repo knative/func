47e1f2ac6ca7e43371d82838ec9a44772b4ce60f
David Fridrich <fridrich.david19@gmail.com>
2025-01-31 17:00:04 +0100

    bump
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -6,7 +6,7 @@ set_versions() {
   kind_node_version=v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
 
   # Can be updated programatically 
-  knative_serving_version="v1.16.0"
-  knative_eventing_versio="v1.16.0"
-  contour_version="v1.16.0"
+  knative_serving_version="v1.17.0"
+  knative_eventing_versio="v1.17.0"
+  contour_version="v1.17.0"
 }
