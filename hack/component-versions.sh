961e46ad6696ceaba4e94ffedc952054835a6174
David Fridrich <fridrich.david19@gmail.com>
2025-02-11 10:07:46 +0100

    add json, add kind version, comments
    Signed-off-by: David Fridrich <fridrich.david19@gmail.com>

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -3,7 +3,7 @@
 set_versions() {
   # Note: Kubernetes Version node image per Kind releases (full hash is suggested):
   # https://github.com/kubernetes-sigs/kind/releases
-  kind_node_version=v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
+  kind_node_version="v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027"
 
   # Can be updated programatically
   knative_serving_version="v1.17.0"
