317e8a04970ceb915bb5ab219ccb031660e9f6fa
Matej Vašek <mvasek@redhat.com>
2025-05-26 19:43:17 +0200

    Fix detection of hostname resolution failure
    It appears that newer version of docker outputs different error when
    registry hostname is not resolvable.
    
    Signed-off-by: Matej Vašek <mvasek@redhat.com>

--- pkg/docker/pusher.go
+++ pkg/docker/pusher.go
@@ -202,7 +202,9 @@ func (n *Pusher) pushImage(ctx context.Context, f fn.Function, credentials Crede
 		return digest, nil
 	}
 	errStr := err.Error()
-	if strings.Contains(errStr, "no such host") || strings.Contains(errStr, "failure in name resolution") {
+	if strings.Contains(errStr, "no such host") ||
+		strings.Contains(errStr, "failure in name resolution") ||
+		regexp.MustCompile(`lookup .*: server misbehaving`).MatchString(errStr) {
 		// push with custom transport to be able to push into cluster private registries
 		return n.push(ctx, f, credentials, output)
 	}
