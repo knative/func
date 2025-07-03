713aabb6666a59bfd59ea3e494be93c6b55be0de
Luke Kingland <luke@lukekingland.com>
2025-07-03 17:14:02 +0900

    chore: use stderr for logging in pipelines pkg (#2912)

--- pkg/pipelines/tekton/templates.go
+++ pkg/pipelines/tekton/templates.go
@@ -245,7 +245,7 @@ func createResource(projectRoot, fileName, fileTemplate string, data interface{}
 			return err
 		}
 		if !overwrite {
-			fmt.Printf(" ⚠️ Pipeline template is not updated in \"%s/%s\"\n", resourcesDirectory, fileName)
+			fmt.Fprintf(os.Stderr, " ⚠️  Pipeline template is not updated in \"%s/%s\"\n", resourcesDirectory, fileName)
 			return nil
 		}
 	}
@@ -259,9 +259,9 @@ func createResource(projectRoot, fileName, fileTemplate string, data interface{}
 	err = tmpl.Execute(file, data)
 	if err == nil {
 		if overwrite {
-			fmt.Printf(" ✅ Pipeline template is updated in \"%s/%s\"\n", resourcesDirectory, fileName)
+			fmt.Fprintf(os.Stderr, " ✅ Pipeline template is updated in \"%s/%s\"\n", resourcesDirectory, fileName)
 		} else {
-			fmt.Printf(" ✅ Pipeline template is created in \"%s/%s\"\n", resourcesDirectory, fileName)
+			fmt.Fprintf(os.Stderr, " ✅ Pipeline template is created in \"%s/%s\"\n", resourcesDirectory, fileName)
 		}
 	}
 	return err
@@ -474,7 +474,7 @@ func createAndApplyResource(projectRoot, fileName, fileTemplate, kind, resourceN
 
 	m, err = m.Transform(manifestival.InjectNamespace(namespace))
 	if err != nil {
-		fmt.Printf("error procesing template: %v", err)
+		fmt.Fprintf(os.Stderr, "error procesing template: %v", err)
 		return err
 	}
 
