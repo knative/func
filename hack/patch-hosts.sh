f157e012eb58d3fbc69bf5a01133f4eefdb86440
Matej Vašek <mvasek@redhat.com>
2025-07-07 16:08:34 +0200

    Make patch-hosts ip version aware
    Add A and AAAA records for localtest.me depending on whether
    the control plane node has IPv4 and/or IPv6 address.
    
    Signed-off-by: Matej Vašek <mvasek@redhat.com>

--- hack/patch-hosts.sh
+++ hack/patch-hosts.sh
@@ -22,14 +22,34 @@ function patch_hosts() {
   echo "${blue}Configuring Magic DNS${reset}"
 
   local cluster_node_addr
+  local cluster_node_addr6
 
   cluster_node_addr="$(docker container inspect func-control-plane | jq ".[0].NetworkSettings.Networks.kind.IPAddress" -r)"
+  cluster_node_addr6="$(docker container inspect func-control-plane | jq ".[0].NetworkSettings.Networks.kind.GlobalIPv6Address" -r)"
+
+  local a_recs=""
+  local aaaa_recs=""
+
+  if [[ -n $cluster_node_addr ]]; then
+          a_recs="\
+localtest.me.            IN      A       ${cluster_node_addr}\n\
+*.localtest.me.          IN      A       ${cluster_node_addr}\n";
+  fi
+
+  if [[ -n $cluster_node_addr6 ]]; then
+          aaaa_recs="\
+localtest.me.            IN      AAAA    ${cluster_node_addr6}\n\
+*.localtest.me.          IN      AAAA    ${cluster_node_addr6}\n";
+  fi
 
   $KUBECTL patch cm/coredns -n kube-system --patch-file /dev/stdin <<EOF
 {
   "data": {
     "Corefile": ".:53 {\n    errors\n    health {\n       lameduck 5s\n    }\n    ready\n    kubernetes cluster.local in-addr.arpa ip6.arpa {\n       pods insecure\n       fallthrough in-addr.arpa ip6.arpa\n       ttl 30\n    }\n    file /etc/coredns/example.db localtest.me\n    prometheus :9153\n    forward . /etc/resolv.conf {\n       max_concurrent 1000\n    }\n    cache 30\n    loop\n    reload\n    loadbalance\n}\n",
-    "example.db": "; localtest.me test file\nlocaltest.me.            IN      SOA     sns.dns.icann.org. noc.dns.icann.org. 2015082541 7200 3600 1209600 3600\nlocaltest.me.            IN      A       ${cluster_node_addr}\n*.localtest.me.          IN      A       ${cluster_node_addr}\n"
+    "example.db": "; localtest.me test file\n\
+localtest.me.            IN      SOA     sns.dns.icann.org. noc.dns.icann.org. 2015082541 7200 3600 1209600 3600\n\
+$a_recs\
+$aaaa_recs"
   }
 }
 EOF
