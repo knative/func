39c321d923c52647d69c2809e1ebd9bf0b60ff78
Knative Automation <automation@knative.team>
2025-10-24 04:01:46 -0400

    upgrade to latest dependencies (#3142)
    bumping knative.dev/client/pkg 6c78af5...a186e11:
      > a186e11 upgrade to latest dependencies (# 2093)
    bumping knative.dev/serving 1928000...a2a2441:
      > a2a2441 Update net-gateway-api nightly (# 16197)
      > 689dbb5 Update net-istio nightly (# 16198)
      > cefd500 Update net-contour nightly (# 16195)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -11,7 +11,7 @@ set_versions() {
 	kind_node_version=v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
 
 	# find source-of-truth in component-versions.json to add/modify components
-	knative_serving_version="v1.19.6"
+	knative_serving_version="v1.19.7"
 	knative_eventing_version="v1.19.6"
 	contour_version="v1.19.5"
 	tekton_version="v1.1.0"
