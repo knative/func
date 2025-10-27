b938c8c1b19e21eb64c035e64fdc76deee867bcf
Stanislav Jakuschevskij <sjakusch@redhat.com>
2025-10-27 11:26:10 +0100

    feat: generate github workflow for remote build
    - Add YAML structure types for workflow generation
    - Generate hard coded remote-build-and-deploy workflow with checkout,
      func cli setup, and deploy steps
    - Validate workflow structure and content in tests
    - Export NewCIConfig for external use
    
    Issue SRVOCF-744
    
    Signed-off-by: Stanislav Jakuschevskij <sjakusch@redhat.com>

--- cmd/config_ci.go
+++ cmd/config_ci.go
@@ -29,13 +29,13 @@ type CIConfig struct {
 }
 
 func NewDefaultCIConfig() CIConfig {
-	return newCIConfig(
+	return NewCIConfig(
 		".github/workflows",
-		"local-build-remote-deploy.yaml",
+		"remote-build-and-deploy.yaml",
 	)
 }
 
-func newCIConfig(workflowDir, workflowFile string) CIConfig {
+func NewCIConfig(workflowDir, workflowFile string) CIConfig {
 	return CIConfig{
 		workflowDir,
 		workflowFile,
@@ -44,11 +44,37 @@ func newCIConfig(workflowDir, workflowFile string) CIConfig {
 	}
 }
 
+type GithubWorkflow struct {
+	Name string           `yaml:"name"`
+	On   WorkflowTriggers `yaml:"on"`
+	Jobs map[string]Job   `yaml:"jobs"`
+}
+
+type WorkflowTriggers struct {
+	Push *PushTrigger `yaml:"push,omitempty"`
+}
+
+type PushTrigger struct {
+	Branches []string `yaml:"branches,omitempty"`
+}
+
+type Job struct {
+	RunsOn string `yaml:"runs-on"`
+	Steps  []Step `yaml:"steps"`
+}
+
+type Step struct {
+	Name string            `yaml:"name,omitempty"`
+	Uses string            `yaml:"uses,omitempty"`
+	Run  string            `yaml:"run,omitempty"`
+	With map[string]string `yaml:"with,omitempty"`
+}
+
 func runConfigCIGithub(
-	loaderSaver common.FunctionLoaderSaver,
+	fnLoaderSaver common.FunctionLoaderSaver,
 	ciConfig CIConfig,
 ) error {
-	f, err := initConfigCommand(loaderSaver)
+	f, err := initConfigCommand(fnLoaderSaver)
 	if err != nil {
 		return err
 	}
@@ -58,7 +84,28 @@ func runConfigCIGithub(
 		return err
 	}
 
-	workflowYamlContent := "hello world"
+	workflowYamlContent := `name: Remote Build and Deploy
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v4
+
+    - name: Install func cli
+      uses: gauron99/knative-func-action@main
+      with:
+        version: knative-v1.19.1
+        name: func
+    
+    - name: Deploy function
+      run: func deploy --remote`
 	fnWorkflowYamlPath := filepath.Join(fnWorkflowDirPath, ciConfig.GithubWorkflowFile)
 	if err := os.WriteFile(fnWorkflowYamlPath, []byte(workflowYamlContent), ciConfig.filePerm); err != nil {
 		return err
