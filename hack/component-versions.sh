72442c2dec8e69a0439580cc2a0e7d8ed9ce6879
Knative Automation <automation@knative.team>
2025-10-30 01:50:27 +0000

    upgrade to latest dependencies
    bumping knative.dev/eventing 1169ba7...1169ba7:
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -12,7 +12,7 @@ set_versions() {
 
 	# find source-of-truth in component-versions.json to add/modify components
 	knative_serving_version="v1.20.0"
-	knative_eventing_version="v1.19.8"
+	knative_eventing_version="v1.20.0"
 	contour_version="v1.20.0"
 	tekton_version="v1.1.0"
 	pac_version="v0.35.2"
