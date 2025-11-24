3f471abbbe7b1ef06939ffda44dae9870eb54cb1
David Fridrich <fridrich.david19@gmail.com>
2025-11-24 15:58:09 +0100

    testing

--- pkg/builders/buildpacks/builder.go
+++ pkg/builders/buildpacks/builder.go
@@ -28,8 +28,8 @@ import (
 // DefaultName when no WithName option is provided to NewBuilder
 const DefaultName = builders.Pack
 
-var DefaultBaseBuilder = "ghcr.io/knative/builder-jammy-base:latest"
-var DefaultTinyBuilder = "ghcr.io/knative/builder-jammy-tiny:latest"
+var DefaultBaseBuilder = "ghcr.io/gauron99/builder-jammy-base:latest"
+var DefaultTinyBuilder = "ghcr.io/gauron99/builder-jammy-tiny:latest"
 
 var (
 	DefaultBuilderImages = map[string]string{
