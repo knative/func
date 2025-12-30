0957b4f56d678d7be22d3026299fa301d766b88e
Knative Automation <automation@knative.team>
2025-12-30 13:32:21 +0000

    upgrade to latest dependencies
    Signed-off-by: Knative Automation <automation@knative.team>

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -13,7 +13,7 @@ set_versions() {
 	# find source-of-truth in component-versions.json to add/modify components
 	knative_serving_version="v1.20.1"
 	knative_eventing_version="v1.20.0"
-	contour_version="v1.20.0"
+	contour_version="v1.20.1"
 	tekton_version="v1.1.0"
 	pac_version="v0.35.2"
 }
