75eab8358428aa9aa239248f6847436f79e34308
Matej Vašek <matejvasek@gmail.com>
2026-01-13 14:25:42 +0100

    WIP
    Signed-off-by: Matej Vašek <matejvasek@gmail.com>

--- pkg/pipelines/tekton/gitlab_int_test.go
+++ pkg/pipelines/tekton/gitlab_int_test.go
@@ -52,14 +52,14 @@ func TestInt_Gitlab(t *testing.T) {
 			"See https://github.com/paketo-buildpacks/nodejs/issues/712")
 	}
 
-	// Skip in CI due to persistent timeout issues regardless of allocated time
-	// Note it does indeed run locally.
-	// TODO: Investigate why GitLab webhook builds are not completing in CI
-	// https://github.com/knative/func/issues/3212
-	if os.Getenv("CI") == "true" || os.Getenv("GITHUB_ACTIONS") == "true" {
-		t.Skip("Skipping GitLab test in CI due to persistent timeout issues. " +
-			"Please run GitLab integration tests locally with 'make test-integration' to verify changes.")
-	}
+	//// Skip in CI due to persistent timeout issues regardless of allocated time
+	//// Note it does indeed run locally.
+	//// TODO: Investigate why GitLab webhook builds are not completing in CI
+	//// https://github.com/knative/func/issues/3212
+	//if os.Getenv("CI") == "true" || os.Getenv("GITHUB_ACTIONS") == "true" {
+	//	t.Skip("Skipping GitLab test in CI due to persistent timeout issues. " +
+	//		"Please run GitLab integration tests locally with 'make test-integration' to verify changes.")
+	//}
 
 	var err error
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
