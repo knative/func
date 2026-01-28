ea1833b3f6de3ce6199649bc7cd3306db55957ec
David Fridrich <fridrich.david19@gmail.com>
2026-01-28 10:35:25 +0100

    update components json

--- hack/component-versions.sh
+++ hack/component-versions.sh
@@ -8,12 +8,12 @@
 set_versions() {
 	# Note: Kubernetes Version node image per Kind releases (full hash is suggested):
 	# https://github.com/kubernetes-sigs/kind/releases
-	kind_node_version=v1.32.0@sha256:c48c62eac5da28cdadcf560d1d8616cfa6783b58f0d94cf63ad1bf49600cb027
+	kind_node_version=v1.34.0@sha256:7416a61b42b1662ca6ca89f02028ac133a309a2a30ba309614e8ec94d976dc5a
 
 	# find source-of-truth in component-versions.json to add/modify components
-	knative_serving_version="v1.20.1"
-	knative_eventing_version="v1.20.0"
-	contour_version="v1.20.1"
+	knative_serving_version="v1.21.0"
+	knative_eventing_version="v1.21.0"
+	contour_version="v1.21.0"
 	tekton_version="v1.1.0"
 	pac_version="v0.35.2"
 }
